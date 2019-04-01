6093f488576c05bb43163e62999309af64e245a6
djperrefort <djperrefort@pitt.edu>
2019-04-01 00:49:46 -0400

    Comments out backup and restore functions

--- broker/orm/__init__.py
+++ broker/orm/__init__.py
@@ -10,21 +10,21 @@ Examples:
 
     >>> from broker.orm import session, SDSS
     >>> session.query(SDSS).all()
-
-    To backup data to a local SQLite file:
-
-    >>>  from broker.orm import backup_to_sqlite
-    >>>  backup_to_sqlite('./backup.db')
-
-    To restore from a backup (via INSERT):
-
-    >>>  from broker.orm import restore_from_sqlite
-    >>>  restore_from_sqlite('./backup.db')
 """
+
+#    To backup data to a local SQLite file:
+#
+#    >>>  from broker.orm import backup_to_sqlite
+#    >>>  backup_to_sqlite('./backup.db')
+#
+#    To restore from a backup (via INSERT):
+#
+#    >>>  from broker.orm import restore_from_sqlite
+#    >>>  restore_from_sqlite('./backup.db')
 
 from ._ingest_catalogs import ingest_sdss
 from ._orm import (SDSS,
-                   backup_to_sqlite,
+                   # backup_to_sqlite,
                    engine,
-                   restore_from_sqlite,
+                   # restore_from_sqlite,
                    session)
