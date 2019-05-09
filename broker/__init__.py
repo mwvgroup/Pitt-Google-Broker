817b71c95d4f0e0fada939e7c0918485093e454b
djperrefort <djperrefort@pitt.edu>
2019-05-08 20:37:20 -0400

    Adds import warning for missing GCP ID

--- broker/__init__.py
+++ broker/__init__.py
@@ -3,4 +3,11 @@
 
 """A ZTF data broker"""
 
+import os as _os
+from warnings import warn as _warn
+
 from . import alert_ingestion, gcp_setup, ztf_archive
+
+if 'BROKER_PROJ_ID' not in _os.environ:
+    _warn('GCP project id is not set in the current environment. Please see '
+          'documentation for instructions on setting BROKER_PROJ_ID')
