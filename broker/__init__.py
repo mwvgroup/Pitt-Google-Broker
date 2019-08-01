e4b50c91b80f6385569505a0d1fddbc5baa6ee50
troyraen <troy.raen@pitt.edu>
2019-08-01 17:28:10 -0400

    updates __init__ to match master, then updated to import value_added

--- broker/__init__.py
+++ broker/__init__.py
@@ -6,20 +6,7 @@ real-time processing for alerts from the  Zwicky Transient Facility (ZTF) and
 the Large Synoptic Survey Telescope (LSST).
 """
 
-import os as _os
-from warnings import warn as _warn
-
 from . import alert_acquisition, data_upload, value_added, ztf_archive
 from ._gcp_setup import setup_gcp
-
-if 'BROKER_PROJ_ID' not in _os.environ:
-    _warn('GCP project id is not set in the current environment. Please see '
-          'documentation for instructions on setting BROKER_PROJ_ID '
-          'in your environment')
-
-if 'GOOGLE_APPLICATION_CREDENTIALS' not in _os.environ:
-    _warn('GCP credentials path is not set in the current environment. Please '
-          'see documentation for instructions on setting'
-          'GOOGLE_APPLICATION_CREDENTIALS in your environment')
 
 __version__ = '0.1.1'
