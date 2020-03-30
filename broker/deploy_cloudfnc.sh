5f736b751f9b6c5fc0224b49e60b546e1ed12ab5
troyraen <troy.raen@pitt.edu>
2020-03-30 13:51:08 -0400

    set the env var GOOGLE_CLOUD_PROJECT explicitly in the gcloud command

--- broker/deploy_cloudfnc.sh
+++ broker/deploy_cloudfnc.sh
@@ -5,10 +5,14 @@
 # ``broker/alert_ingestion/GCS_to_BQ/main.py`` as a Google Cloud Function
 # so that it listens for new Avro files added to a Google Cloud Storage bucket
 # and uploads them to a BigQuery table.
+#
+# Note that the `GOOGLE_CLOUD_PROJECT` environment variable must be set
+# explicitly within the `gcloud` command.
 ###
 
+# NOT SURE OF THE RIGHT WAY TO GET INTO THIS DIRECTORY:
 cd alert_ingestion/GCS_to_BQ
 
 # deploy stream_GCS_to_BQ() to listen to the alert avro bucket
 bucket="${GOOGLE_CLOUD_PROJECT}_alert_avro_bucket"
-gcloud functions deploy stream_GCS_to_BQ --runtime python37 --trigger-resource ${bucket} --trigger-event google.storage.object.finalize
+gcloud functions deploy stream_GCS_to_BQ --runtime python37 --set-env-vars GOOGLE_CLOUD_PROJECT=${GOOGLE_CLOUD_PROJECT} --trigger-resource ${bucket} --trigger-event google.storage.object.finalize
