38b96bbeb6d22c6dfd5ff81890f3e2b59fcac1fe
troyraen <troy.raen@pitt.edu>
2021-02-10 22:01:58 -0500

    implementing Daniel's PR comments

---
+++ broker/cloud_functions/ps_to_gcs/exceptions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""Custom exceptions for ps_to_gcs Cloud Function.
+"""
+
+class CloudConnectionError(Exception):
+    """Error connecting to one or more Google Cloud services"""
+    pass
+
+class SchemaParsingError(Exception):
+    """Error parsing or guessing properties of an alert schema"""
+    pass
