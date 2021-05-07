3a4187d25d5c1eeceed7cad587ab7c235a0a5619
troyraen <troy.raen@pitt.edu>
2021-05-07 19:12:42 -0400

    adds cs_upload_file()

--- broker/broker_utils/broker_utils/gcp_utils.py
+++ broker/broker_utils/broker_utils/gcp_utils.py
@@ -5,7 +5,7 @@ GCP resources.
 """
 
 from google.cloud import bigquery, storage
-from typing import List
+from typing import List, Optional
 
 pgb_project_id = 'ardent-cycling-243415'
 
@@ -25,14 +25,14 @@ def bq_insert_rows(table_id: str, rows: List[dict]):
     errors = bq_client.insert_rows(table, rows)
 
 
-def cs_download_file(localdir: str, bucket_id: str, filename: str =None):
+def cs_download_file(localdir: str, bucket_id: str, filename: Optional[str] = None):
     """
     Args:
         localdir:   Path to local directory where file(s) will be downloaded to.
         bucket_id:  Name of the GCS bucket, not including the project ID.
                     For example, pass 'ztf-alert_avros' for the bucket
                     'ardent-cycling-243415-ztf-alert_avros'.
-        filename:   Name of the file to download (full filename or a prefix).
+        filename:   Name or prefix of the file(s) in the bucket to download.
     """
     # connect to the bucket and get an iterator that finds blobs in the bucket
     storage_client = storage.Client(pgb_project_id)
@@ -46,3 +46,27 @@ def cs_download_file(localdir: str, bucket_id: str, filename: str =None):
         local_path = f'{localdir}/{blob.name}'
         blob.download_to_filename(local_path)
         print(f'Downloaded {local_path}')
+
+def cs_upload_file(local_file: str, bucket_id: str, bucket_filename: Optional[str] = None):
+    """
+    Args:
+        local_file: Path of the file to upload.
+        bucket_id:  Name of the GCS bucket, not including the project ID.
+                    For example, pass 'ztf-alert_avros' for the bucket
+                    'ardent-cycling-243415-ztf-alert_avros'.
+        bucket_filename:    String to name the file in the bucket. If None,
+                            bucket_filename = local_filename.
+    """
+    if bucket_filename is None:
+        bucket_filename = local_file.split('/')[-1]
+
+    # connect to the bucket
+    storage_client = storage.Client(pgb_project_id)
+    bucket_name = f'{pgb_project_id}-{bucket_id}'
+    print(f'Connecting to bucket {bucket_name}')
+    bucket = storage_client.get_bucket(bucket_name)
+
+    # upload
+    blob = bucket.blob(bucket_filename)
+    blob.upload_from_filename(local_file)
+    print(f'Uploaded {local_file} as {bucket_filename}')
