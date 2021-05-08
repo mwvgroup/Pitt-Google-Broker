8a4aa4b49293924933993e8e0b090d1679bd5626
troyraen <troy.raen@pitt.edu>
2021-05-07 20:51:13 -0400

    upload setup_broker dir to broker_files bucket

--- broker/setup_broker/setup_broker.sh
+++ broker/setup_broker/setup_broker.sh
@@ -82,9 +82,10 @@ if [ "$teardown" != "True" ]; then
     echo "Uploading broker files to GCS..."
     o="GSUtil:parallel_process_count=1" # disable multiprocessing for Macs
     gsutil -m -o "$o" cp -r ../beam "gs://${broker_bucket}"
+    gsutil -m -o "$o" cp -r ../broker_utils/schema_maps "gs://${broker_bucket}"
     gsutil -m -o "$o" cp -r ../consumer "gs://${broker_bucket}"
     gsutil -m -o "$o" cp -r ../night_conductor "gs://${broker_bucket}"
-    gsutil -m -o "$o" cp -r ../broker_utils/schema_maps "gs://${broker_bucket}"
+    gsutil -m -o "$o" cp -r ../setup_broker "gs://${broker_bucket}"
 
     #--- Setup the Pub/Sub notifications on ZTF Avro storage bucket
     echo
