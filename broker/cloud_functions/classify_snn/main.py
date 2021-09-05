1768e201321128aee96094dd3c8ef4b5dc7fad11
troyraen <troy.raen@pitt.edu>
2021-09-05 14:54:08 -0400

    change col name pred_class -> predicted_class

--- broker/cloud_functions/classify_snn/main.py
+++ broker/cloud_functions/classify_snn/main.py
@@ -92,7 +92,7 @@ def _classify_with_snn(alert_dict: dict) -> dict:
         schema_map["sourceId"]: snn_df.sourceId,
         "prob_class0": pred_probs[0].item(),
         "prob_class1": pred_probs[1].item(),
-        "pred_class": np.argmax(pred_probs).item(),
+        "predicted_class": np.argmax(pred_probs).item(),
     }
 
     return snn_dict
