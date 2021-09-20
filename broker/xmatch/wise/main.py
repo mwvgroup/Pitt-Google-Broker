6f3ee0fbd94794a37845cbedb4141a3081332554
troyraen <troy.raen@pitt.edu>
2021-09-20 18:03:12 -0400

    cleanup code and comments for clarity

--- broker/xmatch/wise/main.py
+++ broker/xmatch/wise/main.py
@@ -74,8 +74,8 @@ def index():
 
 
 def _xmatch_wise(alert_dict):
-    job_config = _get_job_config(alert_dict)
     sql_stmnt = _get_sql_stmnt()
+    job_config = _get_job_config(alert_dict)
     query_job = gcp_utils.query_bigquery(sql_stmnt, job_config=job_config)
 
     results = {}
@@ -85,49 +85,72 @@ def _xmatch_wise(alert_dict):
     return results
 
 
-def _get_job_config(alert_dict):
-    ra = alert_dict['candidate']['ra']  # deg... convert to longitude?
-    dec = alert_dict['candidate']['dec']  # deg... convert to latitude?
-    # ra, dec = 201.5, -2.6
-    d_sep = 60  # units?
-    limit = 2
-
-    # pad = 0.5
-    pad = 0.00138889  # 5 arcsec converted to degrees
-    polygon = f"Polygon(({ra-pad} {dec-pad},{ra-pad} {dec+pad},{ra+pad} {dec+pad},{ra+pad} {dec-pad}, {ra-pad} {dec-pad}))"
-
-    job_config = bigquery.QueryJobConfig(
-        query_parameters=[
-            bigquery.ScalarQueryParameter("ra", "FLOAT64", ra),
-            bigquery.ScalarQueryParameter("dec", "FLOAT64", dec),
-            bigquery.ScalarQueryParameter("d_sep", "FLOAT64", d_sep),
-            bigquery.ScalarQueryParameter("polygon", "STRING", polygon),
-            bigquery.ScalarQueryParameter("limit", "INT", limit),
-        ]
-    )
-
-    return job_config
-
-
 def _get_sql_stmnt():
-    # what are the units of d?
-    # how to convert alert ra, dec to POINT geometry type?
-    # how to choose Polygon boundaries?
-    # Polygon verticies backwards?
-    # how to parameterize SELECT?
+    """Return the parameterized SQL statement that cross matches the alert to AllWISE.
+
+    The query takes the following parameters:
+        @ra: Alert source right ascension in degrees, transformed to [-180, 180] deg.
+        @dec: Alert source declination in degrees.
+        @max_sep: Maximum separation in arcsec to declare an AllWISE source is a match.
+        @polygon: String representing a Polygon geography; restricts the region of space
+                  within which BigQuery should search for matches.
+        @limit: Maximum number of matches to return (ordered by separation distance).
+    """
+    # Note: ST_DISTANCE() returns the distance (meters) on a sphere with Earth's radius.
+    # 30.8874796235 = meters/arcsec on that sphere.
     sql_stmnt = """
         CREATE TEMP FUNCTION
-          ArcSecondDistance(p1 GEOGRAPHY, p2 GEOGRAPHY,
-            d FLOAT64) AS (ST_DISTANCE(p1,p2) < d * 30.8874796235);
+            ArcSecondDistance(p1 GEOGRAPHY, p2 GEOGRAPHY, max_sep_arcsec FLOAT64)
+                AS (ST_DISTANCE(p1, p2) < max_sep_arcsec * 30.8874796235);
         SELECT source_id, ra, dec, point
         FROM `bigquery-public-data.wise_all_sky_data_release.all_wise`
         WHERE
-          ArcSecondDistance(point, ST_GEOGPOINT(@ra, @dec), @d_sep)
-          AND ST_CONTAINS(ST_GEOGFROMTEXT(@polygon), point)
+            ArcSecondDistance(point, ST_GEOGPOINT(@ra, @dec), @max_sep)
+            AND ST_CONTAINS(ST_GEOGFROMTEXT(@polygon), point)
+        ORDER BY ST_DISTANCE(point, ST_GEOGPOINT(@ra, @dec))
         LIMIT @limit;
     """
 
     return sql_stmnt
+
+
+def _get_job_config(alert_dict):
+    """Return the BigQuery `job_config` for the cross match with AllWISE.
+
+    The `job_config` contains the query parameters for the SQL statement that is
+    returned by `_get_sql_stmnt()`.
+    """
+    # declare the parameters
+    max_sep = 5  # arcsec, max separation to declare a match
+    limit = 3  # max number of matches to return (ordered by separation distance)
+    # alert source position
+    ra, dec = alert_dict['candidate']['ra'], alert_dict['candidate']['dec']
+    if ra > 180.0:
+        ra = ra - 360.0
+    # Polygon that bounds the ra/dec. Restricts the BigQuery search region.
+    arcsec_per_deg = 3600
+    pad = max_sep / arcsec_per_deg
+    polygon = (
+        "Polygon(("
+        f"{ra-pad} {dec-pad},"
+        f"{ra-pad} {dec+pad},"
+        f"{ra+pad} {dec+pad},"
+        f"{ra+pad} {dec-pad},"
+        f"{ra-pad} {dec-pad}"
+        "))"
+    )
+
+    # create and return the job_config
+    job_config = bigquery.QueryJobConfig(
+        query_parameters=[
+            bigquery.ScalarQueryParameter("ra", "FLOAT64", ra),
+            bigquery.ScalarQueryParameter("dec", "FLOAT64", dec),
+            bigquery.ScalarQueryParameter("max_sep", "FLOAT64", max_sep),
+            bigquery.ScalarQueryParameter("polygon", "STRING", polygon),
+            bigquery.ScalarQueryParameter("limit", "INT", limit),
+        ]
+    )
+    return job_config
 
 
 if __name__ == "__main__":
