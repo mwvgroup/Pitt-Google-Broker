878265056eab360cfe35987d35e26a98dfbf2019
troyraen <troy.raen@pitt.edu>
2021-09-27 13:43:27 -0400

    allwise xmatch updates

--- broker/cloud_run/wise/main.py
+++ broker/cloud_run/wise/main.py
@@ -9,10 +9,10 @@ https://wise2.ipac.caltech.edu/docs/release/allwise/
 import base64
 from flask import Flask, request
 from google.cloud import logging, bigquery
-import json
 import os
+from typing import Optional
 
-from broker_utils import gcp_utils, schema_maps
+from broker_utils import data_utils, gcp_utils, schema_maps
 
 
 app = Flask(__name__)
@@ -28,7 +28,7 @@ logger = logging_client.logger(log_name)
 
 # GCP resources used in this module
 bq_dataset = f"{SURVEY}_alerts"
-ps_topic = f"{SURVEY}-alerts"
+ps_topic = f"{SURVEY}-AllWISE"
 if TESTID != "False":  # attach the testid to the names
     bq_dataset = f"{bq_dataset}_{TESTID}"
     ps_topic = f"{ps_topic}-{TESTID}"
@@ -55,7 +55,9 @@ def index():
 
     # unpack the alert
     msg = envelope["message"]
-    alert_dict = json.loads(base64.b64decode(msg["data"]).decode("utf-8"))
+    alert_dict = data_utils.decode_alert(
+        base64.b64decode(msg["data"]), drop_cutouts=True, schema_map=schema_map
+    )
     attrs = {
         schema_map["objectId"]: str(alert_dict[schema_map["objectId"]]),
         schema_map["sourceId"]: str(alert_dict[schema_map["sourceId"]]),
@@ -68,24 +70,25 @@ def index():
     gcp_utils.publish_pubsub(
         ps_topic, {"alert": alert_dict, "AllWISE": wise_dict}, attrs=attrs
     )
-    gcp_utils.insert_rows_bigquery(bq_table, [wise_dict])
+    _store_bigquery(wise_dict, attrs)
 
     return ("", 204)
 
 
 def _xmatch_wise(alert_dict):
-    sql_stmnt = _get_sql_stmnt()
-    job_config = _get_job_config(alert_dict)
-    query_job = gcp_utils.query_bigquery(sql_stmnt, job_config=job_config)
+    """Query the AllWISE catalog for positional matches and return them in a dict."""
+    sql_stmnt = _create_sql_stmnt()
+    job_config = _create_job_config(alert_dict)
+    query_job = _query_bigquery(sql_stmnt, job_config=job_config)
 
     results = {}
     for r, row in enumerate(query_job):
-        results[f'match{r}'] = dict(row)
+        results[f'allwise{r}'] = dict(row)
 
     return results
 
 
-def _get_sql_stmnt():
+def _create_sql_stmnt():
     """Return the parameterized SQL statement that cross matches the alert to AllWISE.
 
     The query takes the following parameters:
@@ -96,13 +99,14 @@ def _get_sql_stmnt():
                   within which BigQuery should search for matches.
         @limit: Maximum number of matches to return (ordered by separation distance).
     """
+    cols = _allwise_cols_to_select()
     # Note: ST_DISTANCE() returns the distance (meters) on a sphere with Earth's radius.
     # 30.8874796235 = meters/arcsec on that sphere.
-    sql_stmnt = """
+    sql_stmnt = f"""
         CREATE TEMP FUNCTION
             ArcSecondDistance(p1 GEOGRAPHY, p2 GEOGRAPHY, max_sep_arcsec FLOAT64)
                 AS (ST_DISTANCE(p1, p2) < max_sep_arcsec * 30.8874796235);
-        SELECT source_id, ra, dec, point
+        SELECT {', '.join(cols)}
         FROM `bigquery-public-data.wise_all_sky_data_release.all_wise`
         WHERE
             ArcSecondDistance(point, ST_GEOGPOINT(@ra, @dec), @max_sep)
@@ -114,11 +118,67 @@ def _get_sql_stmnt():
     return sql_stmnt
 
 
-def _get_job_config(alert_dict):
+def _allwise_cols_to_select():
+    return [
+        # Sexagesimal, equatorial position-based source name in form: hhmmss.ss+ddmmss.s
+        'designation',
+        'ra',  # J2000 right asc. wrt 2MASS PSC ref frame, non-moving src extraction
+        'dec',  # J2000 declination wrt 2MASS PSC ref frame, non-moving src extraction
+        'sigra',  # One-sigma uncertainty in ra from the non-moving source extraction
+        'sigdec',  # One-sigma uncertainty in dec from the non-moving source extraction
+        'cntr',  # Unique ID number for this object in the AllWISE Catalog/Reject Table
+        'source_id',  # Unique source ID
+        # W1 magnitude measured with profile-fitting photometry, or the magnitude of the
+        # 95% confidence brightness upper limit if the W4 flux measurement has SNR<2
+        'w1mpro',
+        'w1sigmpro',  # W1 profile-fit photometric measurement uncertainty in mag units
+        'w1snr',  # W1 profile-fit measurement signal-to-noise ratio
+        'w1rchi2',  # Reduced χ2 of the W1 profile-fit photometry
+        # W2 magnitude measured with profile-fitting photometry, or the magnitude of the
+        # 95% confidence brightness upper limit if the W4 flux measurement has SNR<2
+        'w2mpro',
+        'w2sigmpro',  # W2 profile-fit photometric measurement uncertainty in mag units
+        'w2snr',  # W2 profile-fit measurement signal-to-noise ratio
+        'w2rchi2',  # Reduced χ2 of the W2 profile-fit photometry
+        # W3 magnitude measured with profile-fitting photometry, or the magnitude of the
+        # 95% confidence brightness upper limit if the W4 flux measurement has SNR<2
+        'w3mpro',
+        'w3sigmpro',  # W3 profile-fit photometric measurement uncertainty in mag units
+        'w3snr',  # W3 profile-fit measurement signal-to-noise ratio
+        'w3rchi2',  # Reduced χ2 of the W3 profile-fit photometry
+        # W4 magnitude measured with profile-fitting photometry, or the magnitude of the
+        # 95% confidence brightness upper limit if the W4 flux measurement has SNR<2
+        'w4mpro',
+        'w4sigmpro',  # W4 profile-fit photometric measurement uncertainty in mag units
+        'w4snr',  # W4 profile-fit measurement signal-to-noise ratio
+        'w4rchi2',  # Reduced χ2 of the W4 profile-fit photometry
+        'nb',  # Number of PSF components used simultaneously in profile-fitting the src
+        'na',  # Active deblending flag.
+        'pmra',  # The apparent (not proper!) motion in RA estimated for source
+        'sigpmra',  # Uncertainty in the RA motion estimation.
+        'pmdec',  # The apparent (not proper!) motion in declination estimated for src
+        'sigpmdec',  # Uncertainty in the Dec motion estimated for this source
+        'ext_flg',  # Extended source flag
+        'var_flg',  # Variability flag
+        # Unique ID of closest source in 2MASS Point Source Catalog (PSC)
+        # that falls within 3" of the non-motion fit position
+        'tmass_key',
+        'r_2mass',  # Distance between WISE source and associated 2MASS PSC source
+        'n_2mass',  # num 2MASS PSC entries within a 3" radius of the WISE src position
+        'j_m_2mass',  # 2MASS J-band magnitude or magnitude upper limit
+        'j_msig_2mass',  # 2MASS J-band corrected photometric uncertainty
+        'h_m_2mass',  # 2MASS H-band magnitude or magnitude upper limit
+        'h_msig_2mass',  # 2MASS H-band corrected photometric uncertainty
+        'k_m_2mass',  # 2MASS K_s-band magnitude or magnitude upper limit
+        'k_msig_2mass',  # 2MASS K_s-band corrected photometric uncertainty
+    ]
+
+
+def _create_job_config(alert_dict):
     """Return the BigQuery `job_config` for the cross match with AllWISE.
 
     The `job_config` contains the query parameters for the SQL statement that is
-    returned by `_get_sql_stmnt()`.
+    returned by `_create_sql_stmnt()`.
     """
     # declare the parameters
     max_sep = 5  # arcsec, max separation to declare a match
@@ -147,10 +207,59 @@ def _get_job_config(alert_dict):
             bigquery.ScalarQueryParameter("dec", "FLOAT64", dec),
             bigquery.ScalarQueryParameter("max_sep", "FLOAT64", max_sep),
             bigquery.ScalarQueryParameter("polygon", "STRING", polygon),
-            bigquery.ScalarQueryParameter("limit", "INT", limit),
+            bigquery.ScalarQueryParameter("limit", "INT64", limit),
         ]
     )
     return job_config
+
+
+def _store_bigquery(wise_dict, attrs):
+    """Store the cross match results in BigQuery."""
+    # if there is no match, skip bq storage
+    # if len(wise_dict) == 0:
+    #     return
+
+    # create dict to upload to bigquery
+    store_dict = dict(attrs)
+    # don't store everything. store just enough to re-query the AllWISE catalog.
+    keep_allwise_cols = ['designation', 'cntr', 'source_id']
+    for allwiseN, wdict in wise_dict.items():
+        store_dict.update(
+            {f'{allwiseN}_{k}': v for k, v in wdict.items() if k in keep_allwise_cols}
+        )
+    # store to bigquery
+    gcp_utils.insert_rows_bigquery(bq_table, [store_dict])
+
+
+def _query_bigquery(
+    query: str,
+    project_id: Optional[str] = None,
+    job_config: Optional[bigquery.job.QueryJobConfig] = None,
+):
+    """Query BigQuery.
+
+    REPRODUCED FROM `broker_utils` FOR TESTING PURPOSES.
+
+    Example query:
+        ``
+        query = (
+            f'SELECT * '
+            f'FROM `{project_id}.{dataset}.{table}` '
+            f'WHERE objectId={objectId} '
+        )
+        ``
+
+    Example of working with the query_job:
+        ``
+        for r, row in enumerate(query_job):
+            # row values can be accessed by field name or index
+            print(f"objectId={row[0]}, candid={row['candid']}")
+        ``
+    """
+    bq_client = bigquery.Client()
+    query_job = bq_client.query(query, job_config=job_config)
+
+    return query_job
 
 
 if __name__ == "__main__":
