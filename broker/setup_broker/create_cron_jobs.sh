94ec5fbb228f63435b4fbfc8c08e8d5de443f699
troyraen <troy.raen@pitt.edu>
2021-11-19 02:46:27 -0500

    update pipeline start time to 01:30 utc

--- broker/setup_broker/create_cron_jobs.sh
+++ broker/setup_broker/create_cron_jobs.sh
@@ -32,7 +32,7 @@ if [ "$teardown" = "True" ]; then
 #--- Create jobs that schedule night conductor
 else
     timezone='UTC'  # to avoid daylight savings issues
-    scheduleSTART='0 3 * * *'  # START at 3am UTC/8pm PDT, everyday
+    scheduleSTART='30 1 * * *'  # START at 1:30am UTC/5:30pm PDT, everyday
     scheduleEND='55 13 * * *'  # END at 1:55pm UTC/6:55am PDT, everyday
     msgSTART='START'
     msgEND='END'
