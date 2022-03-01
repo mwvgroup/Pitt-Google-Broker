e7d1bc79bca974621970d909d0fc8e3a57ac53cf
troyraen <troy.raen@pitt.edu>
2022-02-28 20:50:13 -0500

    set the pubsub and kafka topics in consumer startup script

--- broker/consumer/vm_startup.sh
+++ broker/consumer/vm_startup.sh
@@ -12,8 +12,6 @@ baseurl="http://metadata.google.internal/computeMetadata/v1"
 H="Metadata-Flavor: Google"
 PROJECT_ID=$(curl "${baseurl}/project/project-id" -H "${H}")
 zone=$(curl "${baseurl}/instance/zone" -H "${H}")
-PS_TOPIC=$(curl "${baseurl}/instance/attributes/PS_TOPIC" -H "${H}")
-KAFKA_TOPIC=$(curl "${baseurl}/instance/attributes/KAFKA_TOPIC" -H "${H}")
 # parse the survey name and testid from the VM name
 consumerVM=$(curl "${baseurl}/instance/name" -H "${H}")
 survey=$(echo "$consumerVM" | awk -F "-" '{print $1}')
@@ -25,9 +23,11 @@ fi
 
 #--- GCP resources used in this script
 broker_bucket="${PROJECT_ID}-${survey}-broker_files"
+PS_TOPIC="${survey}-alerts"
 # use test resources, if requested
 if [ "$testid" != "False" ]; then
     broker_bucket="${broker_bucket}-${testid}"
+    PS_TOPIC="${PS_TOPIC}-${testid}"
 fi
 
 #--- Files this script will write
@@ -40,14 +40,23 @@ cd ${brokerdir}
 find ${workingdir} -type f -not -name 'pitt-reader.user.keytab' -delete
 # download fresh files
 gsutil -m cp -r "gs://${broker_bucket}/consumer" .
+gsutil -m cp -r "gs://${broker_bucket}/schema_maps" .
+
+# Kafka topic name
+kafka_topic_syntax=$(yq ".TOPIC_SYNTAX" "${brokerdir}/schema_maps/${survey}.yaml")
+yyyymmdd=$(date -u '+%Y%m%d')
+KAFKA_TOPIC=$(echo "${kafka_topic_syntax}" | sed "s/yyyymmdd/${yyyymmdd}/" )
 
 cd ${workingdir}
 
-#--- Set project and topic configs using the instance metadata
+#--- Set the connector's configs (project and topics)
 fconfig=ps-connector.properties
 sed -i "s/PROJECT_ID/${PROJECT_ID}/g" ${fconfig}
 sed -i "s/PS_TOPIC/${PS_TOPIC}/g" ${fconfig}
 sed -i "s/KAFKA_TOPIC/${KAFKA_TOPIC}/g" ${fconfig}
+# set VM metadata, just for clarity and easy viewing
+gcloud compute instances add-metadata "$consumerVM" --zone "$zone" \
+    --metadata="PS_TOPIC=${PS_TOPIC},KAFKA_TOPIC=${KAFKA_TOPIC}"
 
 #--- Check until alerts start streaming into the topic
 alerts_flowing=false
