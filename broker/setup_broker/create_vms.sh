8cfff624a663b1033bfb46cba7443374cc0e7ee8
troyraen <troy.raen@pitt.edu>
2022-03-02 18:11:29 -0500

    rename instance schedules and disable them on testing instances

--- broker/setup_broker/create_vms.sh
+++ broker/setup_broker/create_vms.sh
@@ -14,11 +14,15 @@ zone="${CE_ZONE:-us-central1-a}" # use env variable CE_ZONE if it exists
 
 #--- GCP resources used in this script
 consumerVM="${survey}-consumer"
+consumerVMsched="${consumerVM}-schedule"
 nconductVM="${survey}-night-conductor"
+nconductVMsched="${nconductVM}-schedule"
 # use test resources, if requested
 if [ "$testid" != "False" ]; then
     consumerVM="${consumerVM}-${testid}"
+    consumerVMsched="${consumerVMsched}-${testid}"
     nconductVM="${nconductVM}-${testid}"
+    nconductVMsched="${nconductVMsched}-${testid}"
 fi
 
 #--- Teardown resources
@@ -33,7 +37,7 @@ else
 #--- Night Conductor VM
     # create schedule
     start_schedule='00 16 * * *'  # 4:00pm UTC / 9:00am PDT, everyday
-    gcloud compute resource-policies create instance-schedule "${nconductVM}" \
+    gcloud compute resource-policies create instance-schedule "${nconductVMsched}" \
         --description="Start ${nconductVM} each morning." \
         --vm-start-schedule="${start_schedule}" \
         --timezone="UTC"
@@ -41,7 +45,7 @@ else
     installscript="gs://${broker_bucket}/night_conductor/vm_install.sh"
     machinetype=e2-standard-2
     gcloud compute instances create "$nconductVM" \
-        --resource-policies="${nconductVM}" \
+        --resource-policies="${nconductVMsched}" \
         --zone="$zone" \
         --machine-type="$machinetype" \
         --scopes=cloud-platform \
@@ -51,7 +55,7 @@ else
     # create schedule
     start_schedule='30 1 * * *'  # 1:30am UTC / 5:30pm PDT, everyday
     stop_schedule='55 13 * * *'  # 1:55pm UTC / 6:55am PDT, everyday
-    gcloud compute resource-policies create instance-schedule "${consumerVM}" \
+    gcloud compute resource-policies create instance-schedule "${consumerVMsched}" \
     --description="Start ${consumerVM} each night, stop each morning." \
     --vm-start-schedule="${start_schedule}" \
     --vm-stop-schedule="${stop_schedule}" \
@@ -60,10 +64,19 @@ else
     installscript="gs://${broker_bucket}/consumer/vm_install.sh"
     machinetype=e2-standard-2
     gcloud compute instances create "$consumerVM" \
-        --resource-policies="${consumerVM}" \
+        --resource-policies="${consumerVMsched}" \
         --zone="$zone" \
         --machine-type="$machinetype" \
         --scopes=cloud-platform \
         --metadata=google-logging-enabled=true,startup-script-url="$installscript" \
         --tags=ztfport # for the firewall rule to open the port
+
+#--- Disable the schedules for testing instances
+    if [ "$testid" = "False" ]; then
+        gcloud compute instances remove-resource-policies "${nconductVM}" \
+            --resource-policies="${nconductVMsched}"
+        gcloud compute instances remove-resource-policies "${consumerVM}" \
+            --resource-policies="${consumerVMsched}"
+    fi
+
 fi
