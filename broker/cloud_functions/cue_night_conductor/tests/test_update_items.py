1f6d9219ee6424026c70c1228a6d2d91af374263
Daniel <djperrefort@gmail.com>
2022-03-09 22:26:12 -0500

    Adds some placeholder tests

---
+++ broker/cloud_functions/cue_night_conductor/tests/test_update_items.py
@@ -0,0 +1,12 @@
+from unittest import Testcase
+
+from ..main import get_update_items
+
+
+class Test(Testcase):
+    """This is a placeholder test just so the CI pipeline has something to run"""
+
+    def runtest(self):
+        cue = 'END'
+        output = get_update_items(cue, None)
+        self.assertEqual(output[0], [{'key': 'NIGHT', 'value': cue}])
