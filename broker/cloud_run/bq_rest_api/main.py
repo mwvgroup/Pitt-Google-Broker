0fce9cf8f778c833aba94b69fb0587ee458a1e05
Daniel <djperrefort@gmail.com>
2022-03-19 10:19:37 -0400

    Doc updates and code cleanup

--- broker/cloud_run/bq_rest_api/main.py
+++ broker/cloud_run/bq_rest_api/main.py
@@ -1,24 +1,77 @@
+"""
+GCP REST API
+============
+
+A REST interface for querying data from Big Query.
+
+Big Query already provides its own
+`REST API <https://cloud.google.com/bigquery/docs/reference/rest/>`_,
+but the interface isn't flexible enough for our needs.
+This API provides a simpler, read-only interface for table data.
+
+Source Documentation
+--------------------
+
+.. automodule:: pgb_utils.pgb_utils.figures
+   :members:
+"""
+
+from __future__ import annotations
+
 import logging
 import os
-import os
-from dotenv import load_dotenv
+from typing import Type
+
 from flask import Flask
 from flask_marshmallow import Marshmallow, Schema
 from flask_restful import Api, Resource, reqparse
-from sqlalchemy import Table, select, column
-from sqlalchemy import create_engine
+from sqlalchemy import Table, select, column, create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.sql import Select
-from typing import Type
 
 LOG = logging.getLogger('mylogger')
 LOG.setLevel(logging.DEBUG)
-load_dotenv()
 
 LOG.info('Initializing application constructs')
 app = Flask(__name__)  # Handles running the web application
 ma = Marshmallow(app)  # Handles serialization of database objects to dictionaries
 api = Api(app)  # Handles definition of REST endpoints
+
+
+class AutoResource:
+    """A restful flask resource that automatically maps to a database schema
+
+    Instances of this class are responsible for handling incoming GET requests.
+    The generated resource is read-only and only supports GET requests.
+    """
+
+    def __new__(cls, table: Table, schema: Schema, parser: AutoRestParser, session_maker) -> Type[Resource]:
+        """Return a new REST resource
+
+        Args:
+            table: The sqlalchemy table to serve data from
+            schema: The schema used to serialize query data
+
+        Returns:
+            A ``flask_restful.Resource`` class
+        """
+
+        LOG.debug(f'Creating REST resource for table {table.name}')
+
+        class GeneratedResource(Resource):
+            _table = table
+            _schema = schema
+            _parser = parser
+
+            def get(self):
+                query = self._parser.build_query()
+                with session_maker() as session:
+                    result = session.execute(query).all()
+
+                return self._schema.dump(result)
+
+        GeneratedResource.__name__ = table.name.replace('.', '') + 'Resource'
+        return GeneratedResource
 
 
 class AutoSchema:
@@ -29,11 +82,11 @@ class AutoSchema:
     """
 
     def __new__(cls, table: Table, *args, **kwargs) -> Schema:
-        """Return a new marshmallow schema
+        """Return a new marshmallow schema representing a given table
 
         Args:
             table: The sqlalchemy table to generate a schema for
-            Any other arguments supported by the ``Schema`` class
+            Any other arguments supported by the ``marshmallow.Schema`` class
 
         Returns:
             A ``marshmallow.Schema`` instance
@@ -50,13 +103,20 @@ class AutoSchema:
 
 
 class AutoRestParser(reqparse.RequestParser):
-    """Rest argument parser that generates arg options from a table schema
+    """Rest argument parser that generates argument options from a table schema
 
     Instances of this class are responsible for parsing REST arguments and
     converting them into SQL statements.
     """
 
     def __init__(self, table: Table, schema: Schema) -> None:
+        """Create a new REST parser for fetching data from a SQLAlchemy table
+
+        Args:
+            table: The sqlalchemy table to generate a REST parser for
+            schema: The marshmallow schema for the given table
+        """
+
         super().__init__()
         self._table = table
         self._schema = schema
@@ -96,42 +156,6 @@ class AutoRestParser(reqparse.RequestParser):
         return query
 
 
-class AutoResource:
-    """A restful flask resource that automatically maps to a database schema
-
-    Instances of this class are responsible for handling incoming GET requests.
-    The generated resource is read-only and only supports GET requests.
-    """
-
-    def __new__(cls, table: Table, schema: Schema, parser: AutoRestParser, session_maker) -> Type[Resource]:
-        """Return a new REST resource
-
-        Args:
-            table: The sqlalchemy table to serve data from
-            schema: The schema used to serialize query data
-
-        Returns:
-            A ``flask_restful.Resource`` class
-        """
-
-        LOG.debug(f'Creating REST resource for table {table.name}')
-
-        class GeneratedResource(Resource):
-            _table = table
-            _schema = schema
-            _parser = parser
-
-            def get(self):
-                query = self._parser.build_query()
-                with session_maker() as session:
-                    result = session.execute(query).all()
-
-                return self._schema.dump(result)
-
-        GeneratedResource.__name__ = table.name.replace('.', '') + 'Resource'
-        return GeneratedResource
-
-
 db_url = 'bigquery://' + os.environ['GOOGLE_CLOUD_PROJECT']
 LOG.info(f'Connecting to {db_url}')
 engine = create_engine(db_url)
@@ -142,15 +166,15 @@ Base = declarative_base(bind=engine)
 Base.metadata.reflect(bind=engine)
 
 LOG.info('Defining API endpoints')
-for table_name, table in Base.metadata.tables.items():
+for table_name, table_obj in Base.metadata.tables.items():
     endpoint_name = table_name.replace('.', '/')
 
-    schema = AutoSchema(table, many=True)
-    parser = AutoRestParser(table, schema)
-    resource = AutoResource(table, schema, parser, Session)
+    _schema = AutoSchema(table_obj, many=True)
+    _parser = AutoRestParser(table_obj, _schema)
+    _resource = AutoResource(table_obj, _schema, _parser, Session)
 
     LOG.debug(f'Creating endpoint for table {table_name} at {endpoint_name}')
-    api.add_resource(resource, f'/{endpoint_name}')
+    api.add_resource(_resource, f'/{endpoint_name}')
 
 LOG.info('Launching application')
 app.run(debug=os.environ.get('DEBUG', False))
