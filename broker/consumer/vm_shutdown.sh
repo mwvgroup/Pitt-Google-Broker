f6dc15641fcdf7b89bc704e747bbbba1405accc8
troyraen <troy.raen@pitt.edu>
2022-04-02 13:33:23 -0400

    only reset forced topics upon shutdown

--- broker/consumer/vm_shutdown.sh
+++ broker/consumer/vm_shutdown.sh
@@ -5,6 +5,6 @@ baseurl="http://metadata.google.internal/computeMetadata/v1"
 H="Metadata-Flavor: Google"
 vm_name="$(curl ${baseurl}/instance/name -H ${H})"
 
-# Unset metadata so there's no unexpected behvaior on startup
-topics="KAFKA_TOPIC_FORCE=,PS_TOPIC=,PS_TOPIC_FORCE=,PS_TOPIC="
+# Unset FORCE topics in metadata so there's no unexpected behvaior on next startup
+topics="KAFKA_TOPIC_FORCE=,PS_TOPIC_FORCE="
 gcloud compute instances add-metadata "${vm_name}" --metadata="${topics}"
