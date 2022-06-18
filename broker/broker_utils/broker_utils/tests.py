5e08a45d2135edd9bb745fb53c30dddcfbc5b49e
troyraen <troy.raen@pitt.edu>
2022-06-18 03:33:39 -0600

    update mock topic

--- broker/broker_utils/broker_utils/tests.py
+++ broker/broker_utils/broker_utils/tests.py
@@ -79,7 +79,7 @@ class TestAlert:
         attrs = {
             self.id_keys.objectId: str(self.ids.objectId),
             self.id_keys.sourceId: str(self.ids.sourceId),
-            "kafka.topic": "mock_topic_from_pittgoogle_tests_TestAlert",
+            "kafka.topic": "mock_topic_pittgoogle_test",
             "kafka.timestamp": str(tkafka),
             "ingest.timestamp": str(tingest),
         }
