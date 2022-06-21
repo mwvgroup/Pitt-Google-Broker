b6850eb8a7cf2fa54720984069064b4d2ff35546
troyraen <troy.raen@pitt.edu>
2022-06-20 19:13:37 -0600

    rewrite test clsses

--- broker/broker_utils/broker_utils/tests.py
+++ broker/broker_utils/broker_utils/tests.py
@@ -1,145 +1,367 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-"""The ``data_utils`` module contains common functions used to manipulate
-survey and broker data.
-"""
+"""Classes and functions used for Pitt-Google broker testing."""
 
-import datetime
+import base64
+from datetime import datetime, timedelta, timezone
+import logging
 import os
 from pathlib import Path
+from typing import NamedTuple, Union
 
 import numpy as np
 
 from .gcp_utils import pull_pubsub
-from .data_utils import load_alert, idUtils
+from .data_utils import load_alert, decode_alert
+from .types import _AlertIds, AlertIds
 
 
-def local_alerts(survey=None, adir=None):
-    """Return a generator of paths to Avro files in the directory `adir`."""
-    if not adir:
-        adir = Path(os.getenv(f"ALERT_DIR_{survey.upper()}"))
-    return (p for p in adir.glob('**/*.avro'))
+logger = logging.getLogger(__name__)
+
+
+class AlertPaths:
+    """Paths to alerts stored locally."""
+
+    def __init__(self, alert_dir: Union[str, Path] = "ztf"):
+        """Load path to alerts directory and initialize variables.
+
+        Args:
+            alert_dir:
+                Either the path to a directory containing alerts or one of the
+                following strings.
+                "ALERT_DIR" or "alert_dir": The path will be obtained from the
+                    environment variable ALERT_DIR.
+                "ztf" or "elasticc": The path will be obtained from the environment
+                    variable given by ``f"ALERT_DIR_{alert_dir.upper()}"``
+        """
+        if alert_dir in ["ALERT_DIR", "alert_dir"]:
+            self.alert_dir = Path(os.getenv("ALERT_DIR"))
+        elif alert_dir in ["ztf", "elasticc"]:
+            self.alert_dir = Path(os.getenv(f"ALERT_DIR_{alert_dir.upper()}"))
+        else:
+            self.alert_dir = Path(alert_dir)
+
+        self.__mygen = None
+
+    @property
+    def gen(self):
+        """Return a new generator of paths to Avro files in self.alert_dir."""
+        return (p for p in self.alert_dir.glob("**/*.avro"))
+
+    @property
+    def path(self):
+        """Return a path to an alert Avro file.
+
+        This can be used to obtain a random path to an alert, or within a loop to
+        continuously iterate over paths in the alert directory.
+
+        The returned path is the next one in self._mygen (a path generator maintained
+        internally).
+        If you want more control (e.g., to ensure that you iterate over all files in
+        the directory and/or do not recieve a duplicate path), use ``AlertPaths().gen``
+        instead.
+        """
+        return next(self._mygen)
+
+    @property
+    def _mygen(self):
+        """Path generator used by self."""
+        if self.__mygen is None:
+            self.__mygen = self.gen
+        return self.__mygen
+
+
+class Mock:
+    """Mock data and objects useful for testing the broker."""
+
+    def __init__(self, **kwargs):
+        """.
+
+        kwargs may contain keys:
+            modules: list of module names to mock
+            test_alert: an instance of TestAlert
+        """
+        self.kwargs = kwargs
+        self._modules = kwargs.get("modules")
+        self._results = None
+        self._my_test_alert = kwargs.get("test_alert")
+        self._id_tuples = None
+        self._attrs = None
+        self._cfinput = None
+
+    # modules to mock
+    @property
+    def modules(self):
+        """List of modules for which mock results should be created.
+
+        If this is set manually, self._results will be recreated to accommodate.
+        """
+        return self._modules
+
+    @modules.setter
+    def modules(self, value):
+        self._modules = value
+        # force recreation of other properties
+        self._results = None
+
+    # mocked results
+    @property
+    def results(self):
+        """Mock results for the pipeline module(s) given by self.modules."""
+        if self._results is None:
+            if self.modules is not None:
+                self._results = self._generate_results()
+
+        return self._results
+
+    def _generate_results(self):
+        results = dict()
+
+        if "SuperNNova" in self.modules:
+            results["SuperNNova"] = self._supernnova_results
+
+        self._results = results
+        return self._results
+
+    @property
+    def _supernnova_results(self):
+        """Return mocked results for SuperNNova module."""
+        prob_class1 = np.random.uniform()
+        return dict(
+            [
+                (self.id_keys.objectId, self.ids.objectId),
+                (self.id_keys.sourceId, self.ids.sourceId),
+                ("prob_class0", 1 - prob_class1),
+                ("prob_class1", prob_class1),
+                ("predicted_class", round(prob_class1)),
+            ]
+        )
+
+    @property
+    def my_test_alert(self):
+        """Instance of TestAlert used by self to generate appropriate data."""
+        if self._my_test_alert is None:
+            kwargs = dict(self.kwargs)
+            path = kwargs.pop("path", AlertPaths().path)
+            schema_map = kwargs.pop("schema_map")
+            self._my_test_alert = TestAlert(path, schema_map, **kwargs)
+        return self._my_test_alert
+
+    # tuples of mocked alert ID keys and IDs
+    @property
+    def id_tuples(self):
+        """Tuples of mock alert ID keys and IDs."""
+        if self._id_tuples is None:
+
+            if self._my_test_alert is None:
+                sid = ("sourceId", f"mocksrc{int(1e9 * np.random.uniform())}")
+                oid = ("objectId", f"mockobj{int(1e9 * np.random.uniform())}")
+
+            else:
+                sid = (
+                    self._my_test_alert.id_keys.sourceId,
+                    str(self._my_test_alert.ids.sourceId),
+                )
+                oid = (
+                    self._my_test_alert.id_keys.objectId,
+                    str(self._my_test_alert.ids.objectId),
+                )
+
+            self._id_tuples = (sid, oid)
+
+        return self._id_tuples
+
+    # mocked attributes
+    @property
+    def attrs(self):
+        """Return a dictionary of mock attributes to be attached to the message."""
+        if self._attrs is None:
+            sid, oid = self.id_tuples
+            # create int POSIX timestamps in microseconds
+            now = datetime.now(timezone.utc)
+            tkafka = int(now.timestamp() * 1e6)
+            # to convert back: datetime.fromtimestamp(tkafka / 1e6)
+            tingest = int((now + timedelta(seconds=0.345678)).timestamp() * 1e6)
+
+            self._attrs = dict(
+                [
+                    sid,
+                    oid,
+                    ("kafka.topic", "mock_topic_pittgoogle_test"),
+                    ("kafka.timestamp", str(tkafka)),
+                    ("ingest.timestamp", str(tingest)),
+                ]
+            )
+        return self._attrs
+
+    @attrs.setter
+    def attrs(self, value):
+        self._attrs = value
+
+    # mocked Cloud Functions input
+    class _CFInput(NamedTuple):
+        """Alert data for input to a Cloud Function."""
+
+        msg: dict
+        context: NamedTuple
+
+    class _CFContext(NamedTuple):
+        """Mock context input for a Cloud Function."""
+
+        event_id = int(1e12 * np.random.uniform())
+        timestamp = (
+            datetime.now(timezone.utc)
+            .isoformat(timespec="milliseconds")
+            .replace("+00:00", "Z")
+        )
+        # event_type = "google.pubsub.topic.publish"
+        event_type = "broker_utils.tests.Mock"
+        resource = "mock_resource"
+
+    @property
+    def cfinput(self):
+        """Mock input to a Cloud Function."""
+        if self._cfinput is None:
+
+            # return immediately if no schema map was provided
+            if self.kwargs.get("schema_map") is None:
+                logger.error("A schema map is required to mock Cloud Function input.")
+                return
+
+            else:
+                self._cfinput = self._CFInput(
+                    msg=dict(
+                        data=base64.b64encode(self.my_test_alert.msg_payload),
+                        attributes=self.my_test_alert.mock.attrs,
+                    ),
+                    context=self._CFContext(),
+                )
+
+        return self._cfinput
 
 
 class TestAlert:
-    """Functions to work with alerts for testing."""
+    """An alert packet and related functions useful for testing the broker."""
 
-    def __init__(self, path, schema_map):
-        """."""
+    def __init__(self, path, schema_map, **kwargs):
+        """.
+
+        kwargs may contain keys:
+            drop_cutouts, publish_as, mock_modules
+        """
         self.path = path
-        self.data = dict(
-            dict=load_alert(path, 'dict', schema_map=schema_map, drop_cutouts=True),
-            bytes=load_alert(path, 'bytes'),
+
+        abytes = load_alert(path, "bytes")
+        adict = decode_alert(
+            abytes, drop_cutouts=kwargs.get("drop_cutouts", True), schema_map=schema_map
         )
+        self.data = dict(bytes=abytes, dict=adict)
 
-        id_utils = idUtils(schema_map)
-        self.id_keys = id_utils.get_id_keys()
-        # self.objectId_key = get_key("objectId", schema_map)
-        # self.sourceId_key = get_key("sourceId", schema_map)
-        self.ids = id_utils.get_ids(alert_dict=self.data["dict"])
-        # self.objectId = get_value("objectId", self.data["dict"], schema_map)
-        # self.sourceId = get_value("sourceId", self.data["dict"], schema_map)
+        aids = AlertIds(schema_map, alert_dict=self.data["dict"])
+        self.id_keys = aids.id_keys
+        self.ids = aids.ids
 
-    def create_msg_payload(self, publish_as='json', mock=None):
-        """Create a Pub/Sub message payload."""
-        atype = {"json": "dict", "avro": "bytes"}  # alert format
-        alert = self.data[atype[publish_as]]
-        mock_results = self.create_mock_results(mock)
+        self._publish_as = kwargs.get("publish_as", "json")
+        self._msg_payload = None
 
-        if publish_as == "avro":
-            payload = alert
-        else:
-            payload = dict(alert=alert, **mock_results)
+        self.mock_modules = kwargs.get("mock_modules", None)
+        self._mock = None
 
-        return payload
+    # message payload
+    @property
+    def msg_payload(self):
+        """Pub/Sub message payload containing the alert and any mocked results."""
+        if self._msg_payload is None:
+            # create the message payload
+            # alert data type appropriate for the format given by self.publish_as
+            dtype = {"json": "dict", "avro": "bytes"}
+            alert = self.data[dtype[self.publish_as]]
 
-    def create_mock_results(self, modules=None):
-        """Create mock results for a pipeline module."""
-        mock_results = dict()
+            if self.publish_as == "avro":
+                self._msg_payload = alert
 
-        if modules and "SuperNNova" in modules:
-            prob_class1 = np.random.uniform()
-            mock_results["SuperNNova"] = {
-                self.id_keys.objectId: self.ids.objectId,
-                self.id_keys.sourceId: self.ids.sourceId,
-                "prob_class0": 1-prob_class1,
-                "prob_class1": prob_class1,
-                "predicted_class": round(prob_class1),
-            }
+                # currently can't publish an Avro serialized message with mock results
+                if self.mock_modules is not None:
+                    logger.warning(
+                        (
+                            "The published message will be an Avro serialized alert. "
+                            "Mocked results will NOT be attached."
+                        )
+                    )
 
-        return mock_results
+            else:
+                if self.mock_modules is None:
+                    self._msg_payload = alert
 
-    def create_msg_attrs(self):
-        """Return a dictionary of mock attributes to be attached to the message."""
-        # create int POSIX timestamps in microseconds
-        now = datetime.datetime.now(datetime.timezone.utc)
-        tkafka = int(now.timestamp() * 1e6)
-        # to convert back: datetime.datetime.fromtimestamp(tkafka / 1e6)
-        tingest = int((now + datetime.timedelta(seconds=0.345678)).timestamp() * 1e6)
-        attrs = {
-            self.id_keys.objectId: str(self.ids.objectId),
-            self.id_keys.sourceId: str(self.ids.sourceId),
-            "kafka.topic": "mock_topic_pittgoogle_test",
-            "kafka.timestamp": str(tkafka),
-            "ingest.timestamp": str(tingest),
-        }
-        return attrs
+                else:
+                    self._msg_payload = dict(alert=alert, **self.mock.results)
 
-    @staticmethod
-    def guess_publish_format(topic):
-        """Use the topic to guess the format that the message is expected to be in."""
+        return self._msg_payload
+
+    # message publish format
+    @property
+    def publish_as(self):
+        """One of "json" or "avro". Determines the format of the published message.
+
+        If this is set manually, self._msg_payload will be recreated to accommodate.
+        """
+        return self._publish_as
+
+    @publish_as.setter
+    def publish_as(self, value):
+        self._publish_as = value
+        # force recreation of other properties
+        self._msg_payload = None
+
+    def guess_publish_format(self, topic):
+        """Use the topic to kguess the format that the message is expected to be in."""
         avro_topics = ["alerts"]  # all others are json
+
         try:
-            topic_name_stub = topic.split('-')[1]
+            topic_name_stub = topic.split("-")[1]
         except IndexError:
             topic_name_stub = topic
-        publish_as = "avro" if topic_name_stub in avro_topics else "json"
-        return publish_as
+
+        self._publish_as = "avro" if topic_name_stub in avro_topics else "json"
+        return self._publish_as
+
+    @property
+    def mock(self):
+        """Mock data."""
+        if self._mock is None:
+            self._mock = Mock(modules=self.mock_modules)
+        return self._mock
 
 
 class TestValidator:
     """Functions to validate an integration test."""
 
-    def __init__(
-        self, subscrip, published_alert_ids, schema_map, max_pulls=3, ids_in="attrs"
-    ):
-        """Initialize variables."""
+    def __init__(self, subscrip, published_alert_ids, schema_map, **kwargs):
+        """Initialize variables.
+
+        kwargs can include keys:
+            purge_subscrip
+            max_pulls
+        """
         self.subscrip = subscrip
+        self.max_pulls = kwargs.get("max_pulls", 4)
+        self.pulled_msg_ids = None
         self.published_alert_ids = published_alert_ids
-        self.id_utils = idUtils(schema_map)
-        self.max_pulls = max_pulls
-        # attrs or filename. use filename if pulling from a stream produced by a gcs bucket
-        self.ids_in = ids_in
+        self.unmatched_ids = None
+        self.alert_ids = AlertIds(schema_map)  # load once, use to extract all msg IDs
 
-    def pull_and_compare_ids(self):
+    @property
+    def run(self):
         """Pull the subscription and validate that ids match the published alerts."""
-        pulled_msg_ids = self._pull()
-
-        # _pull() gets the ids from the message attributes or filenames
-        # which are always strings.
-        # convert the published_alert_ids to the same type.
-        published_alert_ids = [
-            self.id_utils.ids_to_strings(ids) for ids in self.published_alert_ids
-        ]
-
-        # compare with published alerts
-        diff = set(published_alert_ids).difference(set(pulled_msg_ids))
-        success = len(diff) == 0
-        fmsgsub = f"The message ids pulled from subscription {self.subscrip}"
-        if success:
-            print(f"Success! {fmsgsub} match the input.")
-        else:
-            print(f"Something went wrong. {fmsgsub}  do not match the input.")
-
-        return (success, pulled_msg_ids)
+        self.pulled_msg_ids = self._pull()
+        self.success, self.unmatched_ids = self._compare_ids()
+        return self.success
 
     def _pull(self):
         pulled_msg_ids, i = [], 0
         while len(pulled_msg_ids) < len(self.published_alert_ids):
             max_messages = len(self.published_alert_ids) - len(pulled_msg_ids)
-            msgs = pull_pubsub(
-                self.subscrip, max_messages=max_messages, msg_only=False
-            )
+            msgs = pull_pubsub(self.subscrip, max_messages=max_messages, msg_only=False)
             pulled_msg_ids += self._extract_ids(msgs)
 
             i += 1
@@ -149,14 +371,61 @@ class TestValidator:
         return pulled_msg_ids
 
     def _extract_ids(self, msgs):
-        if self.ids_in == "attrs":
+        """Use this instance's AlertIds instance to extract the alert IDs from msgs.
+
+        This method first guesses whether the IDs should be extracted from message
+        attributes or alert filenames by checking the objectId attribute of the first
+        message in msgs.
+        Background: Messages generated by a GCS bucket contain an attribute called
+        objectId which contains the filename of the object (i.e., file) in the bucket
+        that triggered the message. This is a name collision with the alert's DIA
+        objectId, which broker pipeline modules place in an attribute of the same name.
+        """
+        oid0 = msgs[0].message.attributes["objectId"]
+        oid_is_filename = oid0.split(".")[-1] == "avro"
+
+        if oid_is_filename:
             pulled_msg_ids = [
-                self.id_utils.get_ids(attrs=msg.message.attributes) for msg in msgs
+                self.alert_ids.extract_ids(filename=msg.message.attributes["objectId"])
+                for msg in msgs
             ]
 
-        elif self.ids_in == "filename":
+        else:
             pulled_msg_ids = [
-                self.id_utils.get_ids(filename=msg.message.attributes["objectId"]) for msg in msgs
+                self.alert_ids.extract_ids(attrs=msg.message.attributes) for msg in msgs
             ]
 
         return pulled_msg_ids
+
+    def _compare_ids(self):
+        pulids = set(self.pulled_msg_ids)
+        # _pull() gets the ids from the message attributes or filenames
+        # which are always strings.
+        # convert the published_alert_ids to the same type.
+        pubids = set(
+            [_AlertIds(str(id) for id in ids) for ids in self.published_alert_ids]
+        )
+
+        # compare ID sets
+        unmatched_ids = pulids.symmetric_difference(pubids)
+        success = len(unmatched_ids) == 0
+
+        # log results
+        tmp = f"The message IDs pulled from subscription {self.subscrip}"
+        if success:
+            logger.info(f"Success! {tmp} match the input.")
+        else:
+            logger.debug(f"Something went wrong. {tmp} do not match the input.")
+
+        # warn if pulids contains IDs that were not published
+        if len(pulids.difference(pubids)) > 0:
+            logger.warning(
+                (
+                    "Some IDs were pulled that were not reported as published. "
+                    "You may want to purge the subcription using\n"
+                    f"\tbroker_utils.gcp_utils.purge_subscription({self.subscrip})\n"
+                    "and run the test again."
+                )
+            )
+
+        return success, unmatched_ids
