469178926bbd89d8fd4cc363700bf8a11fb4e132
troyraen <troy.raen@pitt.edu>
2022-08-19 09:33:46 -0700

    put registered schemas in docstring

--- broker/broker_utils/broker_utils/avro_schemas/load.py
+++ broker/broker_utils/broker_utils/avro_schemas/load.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
-"""The ``avro_schemas.load`` module loads and returns Avro schemas."""
+"""The ``avro_schemas.load`` module loads and returns Avro schemas.
+
+
+REGISTERED_SCHEMAS = [
+    "elasticc.v0_9.alert.avsc",
+    "elasticc.v0_9.brokerClassification.avsc",
+]
+"""
 
 from importlib.resources import path as rpath
 import fastavro
