33dcdb94bf2d0c910bcd4a392ee1668acfaa8da8
troyraen <troy.raen@pitt.edu>
2022-09-24 18:01:24 -0700

    double quote bash variables

--- broker/consumer/vm_shutdown.sh
+++ broker/consumer/vm_shutdown.sh
@@ -3,7 +3,7 @@
 # Get VM name
 baseurl="http://metadata.google.internal/computeMetadata/v1"
 H="Metadata-Flavor: Google"
-vm_name="$(curl ${baseurl}/instance/name -H ${H})"
+vm_name="$(curl "${baseurl}/instance/name" -H "${H}")"
 
 # Unset FORCE topics in metadata so there's no unexpected behvaior on next startup
 topics="KAFKA_TOPIC_FORCE=,PS_TOPIC_FORCE="
