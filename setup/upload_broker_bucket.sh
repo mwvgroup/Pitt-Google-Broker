a64bc1017828c03c23a5621814299229542e37fd
troyraen <troy.raen@pitt.edu>
2022-10-01 04:48:38 -0700

    add schema_maps

--- setup/upload_broker_bucket.sh
+++ setup/upload_broker_bucket.sh
@@ -5,4 +5,5 @@ echo
 echo "Uploading broker files to GCS..."
 o="GSUtil:parallel_process_count=1" # disable multiprocessing for Macs
 gsutil -m -o "${o}" cp -r ../broker/consumer "gs://${broker_bucket}"
+gsutil -m -o "${o}" cp -r ../schema_maps "gs://${broker_bucket}"
 gsutil -m -o "${o}" cp -r ../setup "gs://${broker_bucket}"
