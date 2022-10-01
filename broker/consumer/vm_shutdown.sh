e1e92735ae59516e6b10b0e62d4661b81ed41c4c
troyraen <troy.raen@pitt.edu>
2022-10-01 08:58:22 -0700

    clarify VM metadata

--- broker/consumer/vm_shutdown.sh
+++ broker/consumer/vm_shutdown.sh
@@ -6,6 +6,10 @@ H="Metadata-Flavor: Google"
 vm_name=$(curl "${baseurl}/instance/name" -H "${H}")
 zone=$(curl "${baseurl}/instance/zone" -H "${H}")
 
-# Unset topics in metadata so there's no unexpected behvaior on next startup
-topics="KAFKA_TOPIC=,KAFKA_TOPIC_FORCE=,PS_TOPIC=,PS_TOPIC_FORCE="
+# clear the forced topics so there's no unexpected behvaior on next startup
+topics="KAFKA_TOPIC_FORCE=,PS_TOPIC_FORCE="
 gcloud compute instances add-metadata "${vm_name}" --zone "${zone}" --metadata="${topics}"
+
+# these have no effect when set manually. delete them to avoid confusion.
+topics="CURRENT_KAFKA_TOPIC,CURRENT_PS_TOPIC"
+gcloud compute instances remove-metadata "${vm_name}" --zone "${zone}" --keys="${topics}"
