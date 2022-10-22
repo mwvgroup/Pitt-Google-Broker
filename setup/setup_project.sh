d0dec356cc44a74f8c59efd81e25d7a6eb344837
troyraen <troy.raen@pitt.edu>
2022-10-22 09:26:43 -0700

    add IAM role to start/stop VMs and bind it to compute service account

--- setup/setup_project.sh
+++ setup/setup_project.sh
@@ -4,6 +4,10 @@
 survey="${1:-elasticc}"
 
 region="${CLOUDSDK_COMPUTE_REGION:-us-central1}"
+PROJECT_NUMBER=$(gcloud projects list \
+    --filter="$(gcloud config get-value project)" \
+    --format="value(PROJECT_NUMBER)" \
+    )
 
 #--- Create a firewall rule to open the port used by Kafka
 # Implement this rule on a VM instance using the flag --tags=kafkaport
@@ -20,6 +24,20 @@ region="${CLOUDSDK_COMPUTE_REGION:-us-central1}"
 role_id="userPublic"
 role_yaml="role_user_public.yml"
 gcloud iam roles create "${role_id}" --project="${GOOGLE_CLOUD_PROJECT}" --file="${role_yaml}"
+
+
+# Create an IAM role to start/stop VMs and bind it to the compute engine service account
+role_id="computeStartStop"
+role_yaml="role_compute_startstop.yml"
+gcloud iam roles create "${role_id}" --project="${GOOGLE_CLOUD_PROJECT}" --file="${role_yaml}"
+# read the following add-iam-policy-binding command like this:
+# gcloud <resourceType> add-iam-policy-binding <resourceName> \
+    # --member=<accountToGrantOnTheResource> \
+    # --role=<roleToGrantOnTheResource>
+# credit: https://stackoverflow.com/questions/61875357/gcloud-confusion-around-add-iam-policy-binding
+gcloud projects add-iam-policy-binding "${GOOGLE_CLOUD_PROJECT}" \
+    --member="serviceAccount:service-${PROJECT_NUMBER}@compute-system.iam.gserviceaccount.com" \
+    --role="projects/${GOOGLE_CLOUD_PROJECT}/roles/computeStartStop"
 
 # Create a schedule for the Consumer VM
 consumerVMsched="${survey}-consumer-schedule"
