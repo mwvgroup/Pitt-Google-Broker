979036ee0ecb6492f3eec253cb8029a66913829e
Chris Hernandez <hernandezchristopher.13@gmail.com>
2023-04-20 09:39:59 -0400

    user confirms settings before executing deployment

--- broker/cloud_functions/classify_snn/deployment.sh
+++ broker/cloud_functions/classify_snn/deployment.sh
@@ -12,33 +12,54 @@ survey="${3:-ztf}"
 versiontag="${4:-v3_3}"
 zone="${CE_ZONE:-us-central1-a}" # use env variable CE_ZONE if it exists
 
+#--- Make the user confirm the settings
+echo
+echo "deployment.sh will run with the following configs: "
+echo
+echo "testid = ${testid}"
+echo "teardown = ${teardown}"
+echo "survey = ${survey}"
+echo "versiontag = ${versiontag}"
+echo "zone = ${zone}"
+echo
+echo "Continue? [y/(n)]: "
+
+read input
+input="${input:-n}"
+if [ "${input}" != "y" ]; then
+    echo "Exiting setup."
+    echo
+    exit
+fi
+
 #--- GCP resources used in this script
 classify_snn_trigger_topic="${survey}-tagged"
 classify_snn_CF_name="${survey}-classify_with_SuperNNova"
 # use test resources, if requested
-if [ "$testid" != "False" ]; then
+if [ "${testid}" != "False" ]; then
     classify_snn_trigger_topic="${classify_snn_trigger_topic}-${testid}"
     classify_snn_CF_name="${classify_snn_CF_name}-${testid}"
 fi
 
-if [ "$teardown" = "True" ]; then
+if [ "${teardown}" = "True" ]; then
     # ensure that we do not teardown production resources
-    if [ "$testid" != "False" ]; then
-        gcloud functions delete "$classify_snn_CF_name"
+    if [ "${testid}" != "False" ]; then
+        echo "Deleting Cloud Function: ${classify_snn_CF_name}"
+        gcloud functions delete "${classify_snn_CF_name}"
     fi
 
 else # Deploy the Cloud Functions
 
 #--- classify with SNN cloud function
-    echo "Deploying Cloud Function: $classify_snn_CF_name"
+    echo "Deploying Cloud Function: ${classify_snn_CF_name}"
     classify_snn_entry_point="run"
     memory=512MB  # standard 256MB is too small here
 
-    gcloud functions deploy "$classify_snn_CF_name" \
-        --entry-point "$classify_snn_entry_point" \
-        --memory "$memory" \
+    gcloud functions deploy "${classify_snn_CF_name}" \
+        --entry-point "${classify_snn_entry_point}" \
+        --memory "${memory}" \
         --runtime python37 \
-        --trigger-topic "$classify_snn_trigger_topic" \
+        --trigger-topic "${classify_snn_trigger_topic}" \
         --set-env-vars TESTID="${testid}",SURVEY="${survey}",VERSIONTAG="${versiontag}"
 
 fi
