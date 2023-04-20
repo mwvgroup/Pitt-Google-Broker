0a9d2458dc9b054aad4fee9f1f372d9e7a03a6a3
Chris Hernandez <hernandezchristopher.13@gmail.com>
2023-04-20 10:08:00 -0400

    added deployment script for store_BigQuery

---
+++ broker/cloud_functions/store_BigQuery/deployment.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+# Deploys or deletes broker Cloud Function
+# This script will not delete Cloud Functions that are in production
+
+testid="${1:-test}"
+# "False" uses production resources
+# any other string will be appended to the names of all resources
+teardown="${2:-False}"
+# "True" tearsdown/deletes resources, else setup
+survey="${3:-ztf}"
+# name of the survey this broker instance will ingest
+versiontag="${4:-v3_3}"
+zone="${CE_ZONE:-us-central1-a}" # use env variable CE_ZONE if it exists
+
+#--- Make the user confirm the settings
+echo
+echo "deployment.sh will run with the following configs: "
+echo
+echo "testid = ${testid}"
+echo "teardown = ${teardown}"
+echo "survey = ${survey}"
+echo "versiontag = ${versiontag}"
+echo "zone = ${zone}"
+echo
+echo "Continue? [y/(n)]: "
+
+read input
+input="${input:-n}"
+if [ "${input}" != "y" ]; then
+    echo "Exiting setup."
+    echo
+    exit
+fi
+
+#--- GCP resources used in this script
+store_bq_trigger_topic="${survey}-alerts"
+store_bq_CF_name="${survey}-store_in_BigQuery"
+
+# use test resources, if requested
+if [ "${testid}" != "False" ]; then
+    store_bq_trigger_topic="${store_bq_trigger_topic}-${testid}"
+    store_bq_CF_name="${store_bq_CF_name}-${testid}"
+fi
+
+if [ "${teardown}" = "True" ]; then
+    # ensure that we do not teardown production resources
+    if [ "${testid}" != "False" ]; then
+        gcloud functions delete "${store_bq_CF_name}"
+    fi
+
+else # Deploy the Cloud Functions
+#--- BigQuery storage cloud function
+    echo "Deploying Cloud Function: ${store_bq_CF_name}"
+    store_bq_entry_point="run"
+
+    gcloud functions deploy "${store_bq_CF_name}" \
+        --entry-point "${store_bq_entry_point}" \
+        --runtime python37 \
+        --trigger-topic "${store_bq_trigger_topic}" \
+        --set-env-vars TESTID="${testid}",SURVEY="${survey}",VERSIONTAG="${versiontag}"
+fi
