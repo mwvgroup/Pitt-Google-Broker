c95491aeacf50b7fa98644b5df3038234e203749
Chris Hernandez <hernandezchristopher.13@gmail.com>
2023-04-20 10:08:18 -0400

    added deployment script for tag cf

---
+++ broker/cloud_functions/tag/deployment.sh
@@ -0,0 +1,63 @@
+#! /bin/bash
+# Deploys or deletes broker Cloud Function
+# This script will not delete Cloud Functions that are in production
+
+testid="${1:-test}"
+# "False" uses production resources
+# any other string will be appended to the names of all resources
+teardown="${2:-False}"
+# "True" tearsdown/deletes resources, else setup
+survey="${3:-ztf}"
+# name of the survey this broker instance will ingest
+versiontag="${4:-v3_3}"
+zone="${CE_ZONE:-us-central1-a}" # use env variable CE_ZONE if it exists
+
+#--- Make the user confirm the settings
+echo
+echo "deployment.sh will run with the following configs: "
+echo
+echo "testid = ${testid}"
+echo "teardown = ${teardown}"
+echo "survey = ${survey}"
+echo "versiontag = ${versiontag}"
+echo "zone = ${zone}"
+echo
+echo "Continue? [y/(n)]: "
+
+read input
+input="${input:-n}"
+if [ "${input}" != "y" ]; then
+    echo "Exiting setup."
+    echo
+    exit
+fi
+
+#--- GCP resources used in this script
+tag_trigger_topic="${survey}-lite"
+tag_CF_name="${survey}-tag"
+
+# use test resources, if requested
+if [ "$testid" != "False" ]; then
+    tag_trigger_topic="${tag_trigger_topic}-${testid}"
+    tag_CF_name="${tag_CF_name}-${testid}"
+fi
+
+if [ "$teardown" = "True" ]; then
+    # ensure that we do not teardown production resources
+    if [ "$testid" != "False" ]; then
+        gcloud functions delete "$tag_CF_name"
+    fi
+
+else # Deploy the Cloud Functions
+#--- tag alerts cloud function
+    echo "Deploying Cloud Function: $tag_CF_name"
+    tag_entry_point="run"
+    memory=512MB  # standard 256MB is too small here
+
+    gcloud functions deploy "$tag_CF_name" \
+        --entry-point "$tag_entry_point" \
+        --runtime python37 \
+        --memory "$memory" \
+        --trigger-topic "$tag_trigger_topic" \
+        --set-env-vars TESTID="${testid}",SURVEY="${survey}",VERSIONTAG="${versiontag}"
+fi
