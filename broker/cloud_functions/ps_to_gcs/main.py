6ef186f800762f2a244080f7921a911261c676ea
Chris Hernandez <hernandezchristopher.13@gmail.com>
2024-06-21 10:30:05 -0400

    log severity set to `WARNING`

--- broker/cloud_functions/ps_to_gcs/main.py
+++ broker/cloud_functions/ps_to_gcs/main.py
@@ -61,8 +61,8 @@ class TempAlertFile(SpooledTemporaryFile):
 
     def rollover(self) -> None:
         """Move contents of the spooled file from memory onto disk"""
-
-        logger.log_text(f'Alert size exceeded max memory size: {self._max_size}')
+        msg = f'Alert size exceeded max memory size: {self._max_size}'
+        logger.log_text(msg, severity="WARNING")
         super().rollover()
 
     @property
