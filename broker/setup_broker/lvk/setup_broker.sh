176c5f11f07620484df3c3efde992cc3560f0d63
Chris Hernandez <hernandezchristopher.13@gmail.com>
2024-07-03 09:52:03 -0400

    update setup_broker.sh

--- broker/setup_broker/lvk/setup_broker.sh
+++ broker/setup_broker/lvk/setup_broker.sh
@@ -37,60 +37,32 @@ fi
 #--- GCP resources used directly in this script
 broker_bucket="${PROJECT_ID}-${survey}-broker_files"
 bq_dataset="${survey}"
-topic_alerts="${survey}-alerts"
-table="alerts_${versiontag}"
-schema_path="templates/bq_${survey}_${table}_schema.json"
-topic_bigquery="${survey}-BigQuery"
+
 # use test resources, if requested
 if [ "$testid" != "False" ]; then
     broker_bucket="${broker_bucket}-${testid}"
     bq_dataset="${bq_dataset}_${testid}"
-    topic_alerts="${topic_alerts}-${testid}"
-    topic_bigquery="${survey}-BigQuery-${testid}"
 fi
 
 #--- Create (or delete) GCS, Pub/Sub resources
-if [ "${teardown}" != "True" ]; then
-    # create broker bucket and upload files
-    echo "Creating broker_bucket and uploading files..."
-    gsutil mb -b on -l "${region}" "gs://${broker_bucket}"
-    ./upload_broker_bucket.sh "${broker_bucket}"
-
-    # create BigQuery dataset and table
-    echo "Creating BigQuery dataset and table..."
-
-    cd templates || exit
-    
-    bq --location="${region}" mk \
-    --dataset \
-    "${PROJECT_ID}":"${bq_dataset}"
-
-    bq mk \
-    --table \
-    "${PROJECT_ID}":"${bq_dataset}"."${table}" \
-    "${schema_path}"
-
-    cd .. || exit
-
-    # create pubsub
-    echo "Configuring Pub/Sub resources..."
-    gcloud pubsub topics create "${topic_alerts}"
-    gcloud pubsub topics create "${topic_bigquery}"
-
-    # Set IAM policies on resources
-    user="allUsers"
-    roleid="projects/${GOOGLE_CLOUD_PROJECT}/roles/userPublic"
-    gcloud pubsub topics add-iam-policy-binding "${topic_alerts}" --member="${user}" --role="${roleid}"
-
-else
-    # ensure that we do not teardown production resources
-    if [ "${testid}" != "False" ]; then
-        o="GSUtil:parallel_process_count=1" # disable multiprocessing for Macs
-        gsutil -m -o "${o}" rm -r "gs://${broker_bucket}"
-        gcloud pubsub topics delete "${topic_alerts}"
-        gcloud pubsub topics delete "${topic_bigquery}"
-        bq rm -r -f -d "${PROJECT_ID}":"${bq_dataset}"
+echo
+echo "Configuring BigQuery, GCS, Pub/Sub resources..."
+if [ "$testid" != "False" ]; then
+    if [ "$teardown" = "True" ]; then
+        # delete testing resources
+        python3 setup_gcp.py --survey="$survey" --testid="$testid" --teardown --confirmed --versiontag="${versiontag}"
+    else
+        # setup testing resources
+        python3 setup_gcp.py --survey="$survey" --testid="$testid" --confirmed --region="${region}" --versiontag="${versiontag}"
     fi
+else
+    # setup production resources
+    python3 setup_gcp.py --survey="$survey" --production --confirmed --region="${region}"
+fi
+
+#--- finish setting up buckets and dataset
+if [ "$teardown" != "True" ]; then
+    ./upload_broker_bucket.sh "$broker_bucket"
 fi
 
 #--- Create VM instances
