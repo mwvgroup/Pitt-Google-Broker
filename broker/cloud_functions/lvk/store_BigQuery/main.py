1492550fab9ff18eff632a902ccfba09b05f3d8d
Chris Hernandez <hernandezchristopher.13@gmail.com>
2024-08-27 15:32:55 -0400

    update main.py to incorporate pittgoogle-client methods

--- broker/cloud_functions/lvk/store_BigQuery/main.py
+++ broker/cloud_functions/lvk/store_BigQuery/main.py
@@ -3,39 +3,28 @@
 
 """This module stores alert data in BigQuery tables."""
 
-import base64
 import os
-from typing import Dict, Optional
-from pathlib import Path
-
-from google.cloud import functions_v1, logging
-
-from broker_utils import data_utils, gcp_utils, schema_maps
+import pittgoogle
+import base64
+from google.cloud import functions_v1, pubsub_v1, logging
 
 PROJECT_ID = os.getenv("GCP_PROJECT")
-TESTID = os.getenv("TESTID")
 SURVEY = os.getenv("SURVEY")
+TESTID = os.getenv("TESTID")
 VERSIONTAG = os.getenv("VERSIONTAG")
 
 # connect to the cloud logger
-logging_client = logging.Client()
 log_name = "store-bigquery-cloudfnc"  # same log for all broker instances
+logging_client = logging.Client()
 logger = logging_client.logger(log_name)
 
 # GCP resources used in this module
-bq_dataset = SURVEY
-ps_topic = f"{SURVEY}-BigQuery"
-if TESTID != "False":
-    bq_dataset = f"{bq_dataset}_{TESTID}"
-    ps_topic = f"{ps_topic}-{TESTID}"
+pittgoogle_project = pittgoogle.ProjectIds().pittgoogle
+MODULE_NAME = f"alerts_{VERSIONTAG}"
+TABLE = pittgoogle.Table.from_cloud(MODULE_NAME, survey=SURVEY, testid=TESTID)
+TOPIC = pittgoogle.Topic.from_cloud("BigQuery", survey=SURVEY, testid=TESTID, projectid=PROJECT_ID)
 
-schema_dir_name = "schema_maps"
-schema_file_name = f"{SURVEY}.yaml"
-path_to_local_schema_yaml = Path(__file__).resolve().parent / f"{schema_dir_name}/{schema_file_name}"
-schema_map = schema_maps.load_schema_map(SURVEY, TESTID, schema=path_to_local_schema_yaml)
-
-
-def run(msg: dict, context: functions_v1.context.Context) -> None:
+def run(event: dict, context: functions_v1.context.Context) -> None:
     """Send alert data to various BigQuery tables.
 
     Args:
@@ -45,27 +34,33 @@ def run(msg: dict, context: functions_v1.context.Context) -> None:
 
         context: Metadata describing the Cloud Function's trigging event.
     """
-    # extract the alert
-    alert_dict = data_utils.open_alert(
-        base64.b64decode(msg["data"]), drop_cutouts=False, schema_map=schema_map
-    )
     
-    # send the alert to BigQuery tables
-    alert_table = insert_rows_alerts(alert_dict)
+    # decode the base64-encoded message data
+    decoded_data = base64.b64decode(event["data"])
 
+    # create a PubsubMessage-like object with the existing event dictionary
+    pubsub_message = pubsub_v1.types.PubsubMessage(
+        data=decoded_data,
+        attributes=event.get(["attributes"], {})
+    )
+
+    # unpack the alert
+    alert = pittgoogle.Alert.from_msg(msg=pubsub_message,schema_name="default_schema")
+    
+    # send the alert to BigQuery table
+    alert_table = insert_rows_alerts(alert)
+    
     # announce what's been done
-    publish_pubsub(alert_dict, [alert_table])
+    TOPIC.publish(_create_outgoing_alert(alert, alert_table))
 
-
-def insert_rows_alerts(alert_dict: dict):
+def insert_rows_alerts(alert: pittgoogle.alert.Alert):
     """Insert rows into the `alerts` table via the streaming API."""
     # send to bigquery
-    table_id = f"{bq_dataset}.alerts_{VERSIONTAG}"
-    errors = gcp_utils.insert_rows_bigquery(table_id, [alert_dict])
+    errors = TABLE.insert_rows([alert])
 
     # handle errors; if none, save table id for Pub/Sub message
     if len(errors) == 0:
-        table_dict = {"alerts_table": f"{PROJECT_ID}:{table_id}"}
+        table_dict = {"alerts_table": f"{TABLE.id}"}
     else:
         msg = f"Error inserting to alerts table: {errors}"
         logger.log_text(msg, severity="DEBUG")
@@ -73,21 +68,15 @@ def insert_rows_alerts(alert_dict: dict):
 
     return table_dict
 
+def _create_outgoing_alert(alert: pittgoogle.alert.Alert, table_dict: dict) -> pittgoogle.Alert:
 
-def publish_pubsub(alert_dict: dict, table_dicts: Dict[str, Optional[dict]]):
-    """Announce the table storage operation to Pub/Sub."""
-    # collect attributes
     attrs = {
-        schema_map["objectId"]: str(alert_dict[schema_map["objectId"]]),
-        schema_map["url"]: str(alert_dict[schema_map["url"]]),
-        schema_map["type"]: str(alert_dict[schema_map["type"]]),
+        "alert_table": table_dict['alerts_table'],
+        "type": alert.dict['alert_type']
     }
-    for d in table_dicts:
-        for k, v in d.items():
-            attrs[k] = str(v) if v is not None else ""
-
-    # set empty message body; everything is in the attributes
-    msg = b""
-
-    # publish
-    gcp_utils.publish_pubsub(ps_topic, msg, attrs=attrs)
+    
+    alert_out = pittgoogle.Alert.from_dict(
+        payload=alert.dict, attributes=attrs, schema_name="default_schema"
+    )
+    
+    return alert_out
