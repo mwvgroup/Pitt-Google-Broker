6bbc1e28835e79dfa63b9887a73e5ddfbd58eaec
Chris Hernandez <hernandezchristopher.13@gmail.com>
2025-02-17 11:01:04 -0500

    remove unused variable

--- broker/setup_broker/lsst/setup_broker.sh
+++ broker/setup_broker/lsst/setup_broker.sh
@@ -53,7 +53,6 @@ define_GCP_resources() {
 
 #--- GCP resources used directly in this script
 broker_bucket=$(define_GCP_resources "${PROJECT_ID}-${survey}-broker_files")
-bq_dataset=$(define_GCP_resources "${survey}")
 topic_alerts_raw=$(define_GCP_resources "${survey}-alerts_raw")
 subscription_alerts_raw=$(define_GCP_resources "${survey}-alerts_raw-counter")
 topic_alerts=$(define_GCP_resources "${survey}-alerts")
