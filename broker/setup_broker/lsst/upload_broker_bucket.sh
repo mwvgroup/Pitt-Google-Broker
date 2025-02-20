baec6e50c63ef646f91e28d64f8ae125887a963a
Chris Hernandez <hernandezchristopher.13@gmail.com>
2025-02-20 10:01:00 -0500

    specify consumer/lsst dir in upload_broker_bucket.sh

--- broker/setup_broker/lsst/upload_broker_bucket.sh
+++ broker/setup_broker/lsst/upload_broker_bucket.sh
@@ -5,4 +5,4 @@ broker_bucket=$1 # name of GCS bucket where broker files should be staged
 echo
 echo "Uploading broker files to GCS..."
 o="GSUtil:parallel_process_count=1" # disable multiprocessing for Macs
-gsutil -m -o "${o}" cp -r ../../consumer "gs://${broker_bucket}"
+gsutil -m -o "${o}" cp -r ../../consumer/lsst "gs://${broker_bucket}"
