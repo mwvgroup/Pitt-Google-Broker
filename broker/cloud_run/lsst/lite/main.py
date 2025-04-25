6b8350d29530ed21978adaa18c41add62d09c71a
Chris Hernandez <hernandezchristopher.13@gmail.com>
2025-04-25 09:02:28 -0400

    update main.py

--- broker/cloud_run/lsst/lite/main.py
+++ broker/cloud_run/lsst/lite/main.py
@@ -69,28 +69,12 @@ def run():
 def _create_lite_alert(alert: pittgoogle.Alert) -> pittgoogle.Alert:
     """Create a "lite" alert containing a subset of the fields of the original alert packet."""
 
-    object_fields_list = [
-        "diaObjectId",
-        "nearbyObj1",
-        "nearbyObj2",
-        "nearbyObj3",
-        "nearbyObj1Dist",
-        "nearbyObj2Dist",
-        "nearbyObj3Dist",
-        "nearbyObj1LnP",
-        "nearbyObj2LnP",
-        "nearbyObj3LnP",
-        "u_psfFluxErrMean",
-        "g_psfFluxErrMean",
-        "r_psfFluxErrMean",
-        "i_psfFluxErrMean",
-        "z_psfFluxErrMean",
-        "y_psfFluxErrMean",
-    ]
+    # define the fields that will be present in the lite alert
+    source_fields_list = _create_fields_list(field="source")
+    object_fields_list = _create_fields_list(field="object")
+    ss_object_fields_list = _create_fields_list(field="ssobject")
 
-    source_fields_list = _create_source_fields_list(alert)
-
-    # create dictionaries
+    # create lite versions of fields in the original alert packet
     object_lite_dict = _create_lite_dict(
         alert.dict.get(alert.get_key("object")), object_fields_list
     )
@@ -100,48 +84,99 @@ def _create_lite_alert(alert: pittgoogle.Alert) -> pittgoogle.Alert:
     source_lite_dict = _create_lite_dict(
         alert.dict.get(alert.get_key("source")), source_fields_list
     )
+    ssobject_lite_dict = _create_lite_dict(
+        alert.dict.get(alert.get_key("ss_object")), ss_object_fields_list
+    )
+    # create lite dictionary
     alert_lite_dict = {
         alert.get_key("alertid"): alert.alertid,
         alert.get_key("source"): source_lite_dict,
         alert.get_key("prv_sources"): prev_sources_lite_dict,
+        "prvDiaForcedSources": alert.dict.get("prvDiaForcedSources"),
+        "prvDiaNondetectionLimits": alert.dict.get("prvDiaNondetectionLimits"),
         alert.get_key("object"): object_lite_dict,
+        alert.get_key("ss_object"): ssobject_lite_dict,
     }
 
     return pittgoogle.Alert.from_dict(payload=alert_lite_dict, schema_name=f"{SURVEY}")
 
 
-def _create_source_fields_list(alert: pittgoogle.Alert) -> list[str]:
-    """Creates a list of survey-specific field names to be included in the lite alert for the source dictionary."""
-    broker_field_names = [
-        "sourceid",
-        "mjd",
-        "ra",
-        "ra_err",
-        "dec",
-        "dec_err",
-        "flux",
-        "flux_err",
-        "filter",
-    ]
+def _create_fields_list(field: str) -> list[str]:
+    """Creates a list of survey-specific field names that will be included in the lite dictionary."""
+    if field == "source":
+        source_fields_list = [
+            "diaSourceId",
+            "midpointMjdTai",
+            "ra",
+            "raErr",
+            "dec",
+            "decErr",
+            "psfFlux",
+            "psfFluxErr",
+            "band",
+        ]
 
-    return _get_survey_field_names(alert, broker_field_names)
+        return source_fields_list
 
+    elif field == "object":
+        object_fields_list = [
+            "diaObjectId",
+            "nearbyObj1",
+            "nearbyObj2",
+            "nearbyObj3",
+            "nearbyObj1Dist",
+            "nearbyObj2Dist",
+            "nearbyObj3Dist",
+            "nearbyObj1LnP",
+            "nearbyObj2LnP",
+            "nearbyObj3LnP",
+            "u_psfFluxErrMean",
+            "g_psfFluxErrMean",
+            "r_psfFluxErrMean",
+            "i_psfFluxErrMean",
+            "z_psfFluxErrMean",
+            "y_psfFluxErrMean",
+        ]
 
-def _get_survey_field_names(alert: pittgoogle.Alert, broker_field_names: list) -> list[str]:
-    """Returns a list of survey-specific field names to be included in the lite dictionary."""
-    _survey_field_names = [alert.get_key(field) for field in broker_field_names]
+        return object_fields_list
 
-    # fields may be lists, extract the second element
-    survey_field_names = [
-        _survey_field_name[1] if isinstance(_survey_field_name, list) else _survey_field_name
-        for _survey_field_name in _survey_field_names
-    ]
+    elif field == "ssobject":
+        ss_object_fields_list = [
+            "ssObjectId",
+            "firstObservationDate",
+            "arc",
+            "numObs",
+            "MOID",
+            "MOIDEclipticLongitude",
+            "MOIDDeltaV",
+            "u_H",
+            "u_HErr",
+            "u_Chi2",
+            "g_H",
+            "g_HErr",
+            "g_Chi2",
+            "r_H",
+            "r_HErr",
+            "r_Chi2",
+            "i_H",
+            "i_HErr",
+            "i_Chi2",
+            "z_H",
+            "z_HErr",
+            "z_Chi2",
+            "y_H",
+            "y_HErr",
+            "y_Chi2",
+            "medianExtendedness",
+        ]
 
-    return survey_field_names
+        return ss_object_fields_list
 
 
 def _create_lite_dict(alert_dict: dict, field_names: list[str]) -> dict:
     """Returns a lite dictionary containing fields specified in field_names."""
+    if alert_dict is None:
+        return alert_dict
     return {k: v for k, v in alert_dict.items() if k in field_names}
 
 
