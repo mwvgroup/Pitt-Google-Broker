bb0b072204825763f5ecf6df1113902f2a145479
Chris Hernandez <hernandezchristopher.13@gmail.com>
2025-06-09 14:05:40 -0400

    define additional GCP resources

--- broker/cloud_run/lsst/classify_upsilon/deploy.sh
+++ broker/cloud_run/lsst/classify_upsilon/deploy.sh
@@ -21,7 +21,11 @@ define_GCP_resources() {
     local testid_suffix=""
 
     if [ "$testid" != "False" ]; then
-        testid_suffix="-${testid}"
+        if [ "$base_name" = "${survey}" ] || [ "$base_name" = "${survey}_value_added" ]; then
+            testid_suffix="_${testid}"  # complies with BigQuery naming conventions
+        else
+            testid_suffix="-${testid}"
+        fi
     fi
 
     echo "${base_name}${testid_suffix}"
@@ -29,17 +33,26 @@ define_GCP_resources() {
 
 #--- GCP resources used in this script
 artifact_registry_repo=$(define_GCP_resources "${survey}-cloud-run-services")
+bq_dataset=$(define_GCP_resources "${survey}_value_added")
+bq_table="upsilon"
 cr_module_name=$(define_GCP_resources "${survey}-${MODULE_NAME}")  # lower case required by cloud run
 ps_input_subscrip=$(define_GCP_resources "${survey}-upsilon") # pub/sub subscription used to trigger cloud run module
 ps_output_topic=$(define_GCP_resources "${survey}-upsilon")
 runinvoker_svcact="cloud-run-invoker@${PROJECT_ID}.iam.gserviceaccount.com"
 trigger_topic=$(define_GCP_resources "${survey}-lite")
+# topics and subscriptions involved in writing data to BigQuery
+bq_subscription=$(define_GCP_resources "${survey}-${MODULE_NAME}-bigquery-import") # BigQuery subscription
+ps_deadletter_topic=$(define_GCP_resources "${survey}-${MODULE_NAME}-bigquery-import-deadletter")
+ps_deadletter_subscription="${ps_deadletter_topic}"
 
 
 if [ "${teardown}" = "True" ]; then
     # ensure that we do not teardown production resources
     if [ "${testid}" != "False" ]; then
         gcloud pubsub topics delete "${ps_output_topic}"
+        gcloud pubsub topics delete "${ps_deadletter_topic}"
+        gcloud pubsub subscriptions delete "${bq_subscription}"
+        gcloud pubsub subscriptions delete "${ps_deadletter_subscription}"
         gcloud pubsub subscriptions delete "${ps_input_subscrip}"
         gcloud run services delete "${cr_module_name}" --region "${region}"
     fi
@@ -48,6 +61,16 @@ else # Deploy the Cloud Run service
 
 #--- Deploy Cloud Run
     gcloud pubsub topics create "${ps_output_topic}"
+    gcloud pubsub topics create "${ps_deadletter_topic}"
+    gcloud pubsub subscriptions create "${ps_deadletter_subscription}" --topic="${ps_deadletter_topic}"
+    gcloud pubsub subscriptions create "${bq_subscription}" \
+        --topic="${ps_output_topic}" \
+        --bigquery-table="${PROJECT_ID}:${bq_dataset}.${bq_table}" \
+        --use-table-schema \
+        --drop-unknown-fields \
+        --dead-letter-topic="${ps_deadletter_topic}" \
+        --max-delivery-attempts=5 \
+        --dead-letter-topic-project="${PROJECT_ID}"
 
     echo "Creating container image and deploying to Cloud Run..."
     moduledir="."  # assumes deploying what's in our current directory
