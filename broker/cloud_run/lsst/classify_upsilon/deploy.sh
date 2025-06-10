a971d408d079a243e9e7d43e9c92e69793cdec12
Chris Hernandez <hernandezchristopher.13@gmail.com>
2025-06-10 08:07:05 -0400

    update memory allocation

--- broker/cloud_run/lsst/classify_upsilon/deploy.sh
+++ broker/cloud_run/lsst/classify_upsilon/deploy.sh
@@ -79,7 +79,7 @@ else # Deploy the Cloud Run service
         --substitutions="_SURVEY=${survey},_TESTID=${testid},_MODULE_NAME=${cr_module_name},_REPOSITORY=${artifact_registry_repo}" \
         "${moduledir}" | sed -n 's/^Step #2: Service URL: \(.*\)$/\1/p')
 
-    gcloud run services update "${cr_module_name}" --memory 2G --region "${region}"
+    gcloud run services update "${cr_module_name}" --memory 3G --region "${region}"
 
     echo "Creating trigger subscription for Cloud Run..."
     # WARNING:  This is set to retry failed deliveries. If there is a bug in main.py this will
