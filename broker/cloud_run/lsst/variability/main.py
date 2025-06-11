0d63327f1fcc65919e4307a01761a2c8f86a949a
Chris Hernandez <hernandezchristopher.13@gmail.com>
2025-06-11 11:46:58 -0400

    add top-level `alert_lite` key

--- broker/cloud_run/lsst/variability/main.py
+++ broker/cloud_run/lsst/variability/main.py
@@ -75,7 +75,7 @@ def run():
 
     TOPIC.publish(
         pittgoogle.Alert.from_dict(
-            {**alert_lite.dict, "variability": stetsonj_stats},
+            {"alert_lite": alert_lite.dict, "variability": stetsonj_stats},
             attributes={**alert_lite.attributes, **pg_variable},
             schema_name="default",
         )
