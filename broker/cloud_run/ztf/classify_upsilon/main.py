ce226d10a88edb18c536e880422648052a5baea5
Chris Hernandez <hernandezchristopher.13@gmail.com>
2025-07-08 10:41:32 -0400

    incorporate requested changes

--- broker/cloud_run/ztf/classify_upsilon/main.py
+++ broker/cloud_run/ztf/classify_upsilon/main.py
@@ -34,6 +34,7 @@ HTTP_400 = 400  # HTTP code: Bad Request
 TOPIC = pittgoogle.Topic.from_cloud("upsilon", survey=SURVEY, testid=TESTID, projectid=PROJECT_ID)
 
 app = flask.Flask(__name__)
+rf_model = upsilon.load_rf_model()  # load UPSILoN's classification model
 
 
 @app.route(ROUTE_RUN, methods=["POST"])
@@ -58,82 +59,75 @@ def run() -> tuple[str, int]:
     except pittgoogle.exceptions.BadRequest as exc:
         return str(exc), HTTP_400
 
-    # UPSILoN recommends using light curves with more than ~80 data points
-    has_min_detections_in_any_band = any(
-        alert_lite.dict["variability"].get(f"n_detections_{band}_band", 0) >= 80
-        for band in ["g", "r", "i"]
+    alert_lite_df = pd.DataFrame(
+        [alert_lite.dict.get("source")] + (alert_lite.dict.get("prvSources") or [])
     )
 
-    if has_min_detections_in_any_band:
-        # classify and publish results
-        alert_lite_df = _create_lite_dataframe(alert_lite.dict["alert_lite"])
-        upsilon_dict = _classify_with_upsilon(alert_lite_df)
-        TOPIC.publish(
-            pittgoogle.Alert.from_dict(
-                {
-                    "alert_lite": alert_lite.dict,
-                    "upsilon": {
-                        "objectId": alert_lite.dict["alertIds"]["objectId"],
-                        "candid": alert_lite.dict["alertIds"]["sourceId"],
-                        **upsilon_dict,
-                    },
+    # classify and publish results
+    upsilon_dict = _classify_with_upsilon(alert_lite_df)
+    has_min_detections_in_any_band = any(
+        upsilon_dict.get(f"n_data_points_{band_name}_band") >= 80 for band_name in ["g", "r", "i"]
+    )
+    TOPIC.publish(
+        pittgoogle.Alert.from_dict(
+            {
+                "alert_lite": alert_lite.dict,
+                "upsilon": {
+                    "objectId": alert_lite.dict["alertIds"]["objectId"],
+                    "candid": alert_lite.dict["alertIds"]["sourceId"],
+                    **upsilon_dict,
                 },
-                attributes={
-                    **alert_lite.attributes,
-                    "pg_upsilon_g_label": upsilon_dict["g_label"],
-                    "pg_upsilon_g_flag": upsilon_dict["g_flag"],
-                    "pg_upsilon_r_label": upsilon_dict["r_label"],
-                    "pg_upsilon_r_flag": upsilon_dict["r_flag"],
-                    "pg_upsilon_i_label": upsilon_dict["i_label"],
-                    "pg_upsilon_i_flag": upsilon_dict["i_flag"],
-                },
-                schema_name="default",
-            )
+            },
+            attributes={
+                **alert_lite.attributes,
+                "pg_has_min_detections": has_min_detections_in_any_band,
+                "pg_upsilon_g_label": upsilon_dict["g_label"],
+                "pg_upsilon_g_flag": upsilon_dict["g_flag"],
+                "pg_upsilon_r_label": upsilon_dict["r_label"],
+                "pg_upsilon_r_flag": upsilon_dict["r_flag"],
+                "pg_upsilon_i_label": upsilon_dict["i_label"],
+                "pg_upsilon_i_flag": upsilon_dict["i_flag"],
+            },
+            schema_name="default",
         )
+    )
 
     return "", HTTP_204
 
 
-def _classify_with_upsilon(alert_lite_df: pd.Dataframe) -> dict:
-    upsilon_dict = {}
-    bands = alert_lite_df["filter"].map(pittgoogle.utils.ztf_fid_names()).unique()
-    rf_model = upsilon.load_rf_model()  # load UPSILoN's classification model
-    for band in bands:
-        # ---Extract data
-        filter_diaSources = alert_lite_df[alert_lite_df["filter"] == band]
-        mag_gt_zero = filter_diaSources["mag"].to_numpy() > 0
-        # set output to None if data is absent or there are too few data points for this band
-        # limit recommended by UPSILoN
-        if filter_diaSources.empty or mag_gt_zero.sum() <= 80:
-            upsilon_dict[f"{band}_label"] = None
-            upsilon_dict[f"{band}_probability"] = None
-            upsilon_dict[f"{band}_flag"] = None
-            continue
+def _classify_with_upsilon(alert_lite_df: pd.DataFrame) -> dict:
+    """Classify alert with UPSILoN."""
 
+    fid_to_band_name_map = pittgoogle.utils.ztf_fid_names()
+    upsilon_dict = {}
+    for band_name in fid_to_band_name_map.values():
+        upsilon_dict[f"n_data_points_{band_name}_band"] = 0
+        upsilon_dict[f"{band_name}_label"] = None
+        upsilon_dict[f"{band_name}_probability"] = None
+        upsilon_dict[f"{band_name}_flag"] = None
+
+    for fid in alert_lite_df["filter"].unique():
+        band_name = fid_to_band_name_map.get(fid)
+        # ---Extract data
+        filter_candids = alert_lite_df[alert_lite_df["filter"] == fid]
+        mag_gt_zero = filter_candids["mag"].to_numpy() > 0
+        upsilon_dict[f"n_data_points_{band_name}_band"] = mag_gt_zero.sum()
+        # skip band if no detections or too few valid data points.
+        # to avoid scipy's leastsq error: ("input vector length N=7 must not exceed output length M"), we require
+        # that mag_gt_zero.sum() > 7
+        if filter_candids.empty or mag_gt_zero.sum() <= 7:
+            continue
         # ---Extract features
-        date = filter_diaSources["jd"].to_numpy()[mag_gt_zero]
-        mag = filter_diaSources["mag"].to_numpy()[mag_gt_zero]
-        mag_err = filter_diaSources["magerr"].to_numpy()[mag_gt_zero]
+        date = filter_candids["jd"].to_numpy()[mag_gt_zero]
+        mag = filter_candids["mag"].to_numpy()[mag_gt_zero]
+        mag_err = filter_candids["magerr"].to_numpy()[mag_gt_zero]
         e_features = upsilon.ExtractFeatures(date, mag, mag_err)
         e_features.run()
         features = e_features.get_features()
-
         # ---Classify
         label, probability, flag = upsilon.predict(rf_model, features)
-        upsilon_dict[f"{band}_label"] = label
-        upsilon_dict[f"{band}_probability"] = probability
-        upsilon_dict[f"{band}_flag"] = flag
+        upsilon_dict[f"{band_name}_label"] = label
+        upsilon_dict[f"{band_name}_probability"] = probability
+        upsilon_dict[f"{band_name}_flag"] = flag
 
     return upsilon_dict
-
-
-def _create_lite_dataframe(alert_dict: dict) -> pd.DataFrame:
-    """Return a pandas DataFrame containing the source detections."""
-
-    # sources and previous sources are expected to have the same fields
-    sources_df = pd.DataFrame([alert_dict.get("source")] + (alert_dict.get("prvSources") or []))
-    # use nullable integer data type to avoid converting ints to floats for columns in one dataframe but not the other
-    sources_ints = [c for c, v in sources_df.dtypes.items() if v == int]
-    dataframe = sources_df.astype({c: "Int64" for c in sources_ints})
-
-    return dataframe
