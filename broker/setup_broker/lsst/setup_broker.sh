331dc28f1a91db2917c860a2f9b64822f7e91568
Chris Hernandez <hernandezchristopher.13@gmail.com>
2025-09-10 11:43:20 -0400

    update script to create and deploy resources for the Vitune module

--- broker/setup_broker/lsst/setup_broker.sh
+++ broker/setup_broker/lsst/setup_broker.sh
@@ -54,6 +54,7 @@ bq_table_alerts="alerts_${versiontag}"
 bq_table_supernnova="SuperNNova"
 bq_table_upsilon="upsilon"
 bq_table_variability="variability"
+bq_table_vitune="vitune"
 gcs_broker_bucket=$(define_GCP_resources "${PROJECT_ID}-${survey}-broker_files")
 ps_subscription_reservoir=$(define_GCP_resources "${survey}-alerts-reservoir")
 ps_topic_alerts_raw=$(define_GCP_resources "${survey}-alerts_raw")
@@ -90,6 +91,7 @@ manage_resources() {
         (cd templates && bq mk --table "${PROJECT_ID}:${bq_dataset}.${bq_table_supernnova}" "bq_${survey}_${bq_table_supernnova}_schema.json") || exit 5
         (cd templates && bq mk --table "${PROJECT_ID}:${bq_dataset}.${bq_table_variability}" "bq_${survey}_${bq_table_variability}_schema.json") || exit 5
         (cd templates && bq mk --table "${PROJECT_ID}:${bq_dataset}.${bq_table_upsilon}" "bq_${survey}_${bq_table_upsilon}_schema.json") || exit 5
+        (cd templates && bq mk --table "${PROJECT_ID}:${bq_dataset}.${bq_table_vitune}" "bq_${survey}_${bq_table_vitune}_schema.json") || exit 5
         bq update --description "Alert data from LSST. This table is an archive of the lsst-alerts Pub/Sub stream. It has the same schema as the original alert bytes, including nested and repeated fields." "${PROJECT_ID}:${bq_dataset}.${bq_table_alerts}"
         bq update --description "Binary classification results from SuperNNova." "${PROJECT_ID}:${bq_dataset}.${bq_table_supernnova}"
 
@@ -221,5 +223,9 @@ echo "Configuring Cloud Run services..."
 
     #--- upsilon Cloud Run service
     cd .. && cd classify_upsilon
+    ./deploy.sh "${testid}" "${teardown}" "${survey}" "${region}"
+
+    #--- vitune Cloud Run service
+    cd .. && cd classify_vitune
     ./deploy.sh "${testid}" "${teardown}" "${survey}" "${region}"
 ) || exit
