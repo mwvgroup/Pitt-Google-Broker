98ebe0999c5d5b99f3f2347183b9094092fac8b1
Chris Hernandez <hernandezchristopher.13@gmail.com>
2025-10-07 00:28:20 -0400

    execute deployment scripts for Cloud Run services

--- broker/setup_broker/lsst/setup_broker.sh
+++ broker/setup_broker/lsst/setup_broker.sh
@@ -218,14 +218,14 @@ echo "Configuring Cloud Run services..."
     ./deploy.sh "${testid}" "${teardown}" "${survey}" "${region}"
 
     #--- supernnova Cloud Run service
-    # cd .. && cd classify_snn
-    # ./deploy.sh "${testid}" "${teardown}" "${survey}" "${region}"
+    cd .. && cd classify_snn
+    ./deploy.sh "${testid}" "${teardown}" "${survey}" "${region}"
 
-    # #--- variability Cloud Run service
-    # cd .. && cd variability
-    # ./deploy.sh "${testid}" "${teardown}" "${survey}" "${region}"
+    #--- variability Cloud Run service
+    cd .. && cd variability
+    ./deploy.sh "${testid}" "${teardown}" "${survey}" "${region}"
 
-    # #--- upsilon Cloud Run service
-    # cd .. && cd classify_upsilon
-    # ./deploy.sh "${testid}" "${teardown}" "${survey}" "${region}"
+    #--- upsilon Cloud Run service
+    cd .. && cd classify_upsilon
+    ./deploy.sh "${testid}" "${teardown}" "${survey}" "${region}"
 ) || exit
