035629fb3d2cae1c54f4f2874f09711150f3e02d
Chris Hernandez <hernandezchristopher.13@gmail.com>
2026-03-02 14:17:46 -0400

    update path to SMT

--- broker/setup_broker/lsst/setup_broker.sh
+++ broker/setup_broker/lsst/setup_broker.sh
@@ -143,7 +143,7 @@ manage_resources() {
             --max-delivery-attempts=5 \
             --dead-letter-topic-project="${PROJECT_ID}" \
             --message-filter='attributes.schema_version = "'"${versiontag}"'"' \
-            --message-transforms-file=templates/smt_add_top_level_fields.yaml
+            --message-transforms-file=templates/ps_smt_add_top_level_fields.yaml
         # set IAM policies on public Pub/Sub resources
         if [ "$testid" = "False" ]; then
             user="allUsers"
