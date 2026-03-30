2f25cfa419d72418fcf61e8949ed6a3c2c0d2676
Chris Hernandez <hernandezchristopher.13@gmail.com>
2026-03-30 14:04:33 -0400

    require `psfFlux > 0`

--- broker/cloud_run/lsst/lensing/main.py
+++ broker/cloud_run/lsst/lensing/main.py
@@ -243,10 +243,10 @@ def check_supernova_color_magnitude_criteria(
         return -2.5 * np.log10(psfFlux) + 31.4
 
     # extract the 'r' and 'i' band photometry if it exists
-    r_band_photometry = alert_df[alert_df["band"] == "r"]
-    i_band_photometry = alert_df[alert_df["band"] == "i"]
+    r_band_photometry = alert_df[(alert_df["band"] == "r") and (alert_df["psfFlux"] > 0)]
+    i_band_photometry = alert_df[(alert_df["band"] == "i") and (alert_df["psfFlux"] > 0)]
     if r_band_photometry.empty or i_band_photometry.empty:
-        # observations in one of the two required bands does not exist
+        # observations in one of the two required bands with psfFlux > 0 does not exist
         return not_a_candidate
 
     # create a time-aligned DataFrame of paired r/i observations for the diaObject
